3b0f71b58e2db8afd2f3fd53d6e35f4623ab7ebe
SuperFlyXXI <superflyxxi@yahoo.com>
2019-01-13 21:16:38 -0500

    Backup

---
+++ ripFile.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+if [ -z "${INPUT}" ]; then
+	echo "Need to specify INPUT";
+	exit 1;
+fi
+
+if [ -z "${TITLE}" ]; then
+	echo "Need to specify TITLE or YEAR";
+	exit 2;
+fi
+
+if [ -z "${OUTPUT}" ]; then
+	OUTPUT="${TITLE}"
+	if [ ! -z "${SEASON}" ]; then
+		OUTPUT="${OUTPUT} - s${SEASON}e${EPISODE}"
+	fi
+	if [ ! -z "${SUBTITLE}" ]; then
+		OUTPUT="${OUTPUT} - ${SUBTITLE}"
+	fi
+	if [ ! -z "${YEAR}" ]; then
+		OUTPUT="${OUTPUT} (${YEAR})"
+	fi
+fi
+OUTPUT_DIR=${OUTPUT_DIR:-/usr/media/videos/rip}
+
+LOG=${LOG:-y}
+if [[ "${LOG}" == "y" ]]; then
+	LOGFILE=${OUTPUT_DIR}/${OUTPUT}.log
+	exec 1>"${LOGFILE}"
+	exec 2>"${LOGFILE}"
+fi
+
+set -v
+INPUT_EXT="${INPUT: -4}"
+if [[ -d ${INPUT} ]] || [[ ${INPUT_EXT,,} == ".iso" ]]; then
+	echo "Using bluray directory"
+	INPUT=bluray:${INPUT}
+else
+	echo "Using filename"
+fi
+
+if [ -v PLAYLIST ]; then
+	PLAYLIST_ARGS="-playlist ${PLAYLIST}";
+fi
+
+SUBTITLE_TRACK=${SUBTITLE_TRACK:-s?}
+SUBTITLE_FORMAT=${SUBTITLE_FORMAT:-ass}
+SUBTITLE_TRACK_ARGS="-c:s ${SUBTITLE_FORMAT} -map 0:${SUBTITLE_TRACK}"
+
+AUDIO_TRACK=${AUDIO_TRACK:-a}
+# AUDIO_QUALITY=${AUDIO_QUALITY:-576}
+AUDIO_QUALITY=${AUDIO_QUALITY:-2} # Variable Bitrate of 2 is good
+AUDIO_FORMAT=${AUDIO_FORMAT:-aac} # libfdk_aac is for aac highest quality, aac for great quality, and eac3 is Dolby Digital Ex
+# AUDIO_TRACK_ARGS="-c:a ${AUDIO_FORMAT} -q:a ${AUDIO_QUALITY} -map 0:${AUDIO_TRACK}"
+AUDIO_TRACK_ARGS="-c:a ${AUDIO_FORMAT} -q:a ${AUDIO_QUALITY} -map 0:${AUDIO_TRACK}"
+
+HWACCEL=${HWACCEL:-y}
+HWACCEL_ARGS="-hwaccel vaapi "
+if [[ ${HWACCEL} == "y" ]]; then
+	HWACCEL_ARGS="${HWACCEL_ARGS} -hwaccel_output_format vaapi -hwaccel_device /dev/dri/renderD128"
+	if [[ ${DEINTERLACE:-n} == "y" ]]; then
+		DEINTERLACE_ARGS="-vf 'deinterlace_vaapi=rate=field:auto=1'"
+	fi
+fi
+
+VIDEO_TRACK=${VIDEO_TRACK:-v}
+if [[ ${HWACCEL} == "y" ]]; then
+	VIDEO_TRACK_ARGS="-c:v hevc_vaapi -qp 20 -level:v 41 -map 0:${VIDEO_TRACK}"
+else
+	VIDEO_TRACK_ARGS="-c:v libx265 -crf 20 -level:v 41 -map 0:${VIDEO_TRACK}"
+fi
+
+if [[ ${HDR:-n} == "y" ]]; then
+	VIDEO_TRACK_ARGS="-c:v libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10 -map 0:${VIDEO_TRACK}"
+fi
+
+set -x
+
+ffmpeg -${OVERWRITE_FILE:-y} ${HWACCEL_ARGS} \
+	${PLAYLIST_ARGS} -i "${INPUT}" \
+	${VIDEO_TRACK_ARGS} ${DEINTERLACE_ARGS} \
+	${AUDIO_TRACK_ARGS} \
+	${SUBTITLE_TRACK_ARGS} \
+	-metadata "title"="${TITLE}" -metadata "year"=${YEAR} -metadata "subtitle"="${SUBTITLE}" \
+	-metadata "season"="${SEASON}" -metadata "episode"="${EPISODE}" \
+	-f matroska "${OUTPUT_DIR}/${OUTPUT}.mkv"
+
---
+++ ripSeason.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [ -z "${SEASON_DIR}" ]; then
+	echo "Need to specify SEASON_DIR";
+	exit 1;
+fi
+
+RIP_SH=/usr/media/videos/rip/ripFileWip.sh
+
+LOG=${LOG:-n}
+if [[ "${LOG}" == "y" ]]; then
+	LOGFILE=${OUTPUT_DIR}/${OUTPUT}.log
+	exec 1>"${LOGFILE}"
+	exec 2>"${LOGFILE}"
+fi
+
+IFS=$(echo -en "\n\b")
+SEASON_REGEX=${SEASON_REGEX:-".*[sS]\([0-9]\+\).*"}
+EPISODE_REGEX=${EPISODE_REGEX:-".*[eE]\([0-9]\+\).*"}
+TITLE_REGEX=${TITLE_REGEX:-"^\([a-zA-Z0-9'\! ]\+\).*"}
+SUBTITLE_REGEX=${SUBTITLE_REGEX:-"^.*[eE][0-9]\+\(.*\)\..*$"}
+SEASON_OVR=${SEASON}
+EPISODE_OVR=${EPISODE}
+TITLE_OVR=${TITLE}
+SUBTITLE_OVR=${SUBTITLE}
+
+set -x
+for file in $( ls -1 ${SEASON_DIR} ); do
+	SEASON=${SEASON_OVR:-`echo "$file" | sed "s/${SEASON_REGEX}/\1/"`}
+	EPISODE=${EPISODE_OVR:-`echo "$file" | sed "s/${EPISODE_REGEX}/\1/"`}
+	TITLE=${TITLE_OVR:-`echo "$file" | sed "s/${TITLE_REGEX}/\1/" | sed "s/^\ //" | sed "s/\ $//" | sed "s/\ \ /\ /g"`}
+	SUBTITLE=${SUBTITLE_OVR:-`echo "$file" | sed "s/${SUBTITLE_REGEX}/\1/" | sed "s/^\ //" | sed "s/\ $//" | sed "s/\ \ /\ /g"`}
+	OUTPUT_DIR="${SEASON_DIR}" INPUT="${SEASON_DIR}/${file}" SEASON=${SEASON} EPISODE=${EPISODE} TITLE=${TITLE} SUBTITLE=${SUBTITLE} ${RIP_SH}
+	if [[ "${TEST:-n}" == "y" ]]; then
+		exit 0
+	fi
+done
+
