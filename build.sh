2abd22fa1afe469ff4160657ae6f2d38d0803751
SuperFlyXXI <superflyxxi@yahoo.com>
2019-06-30 15:39:02 -0400

    Fix build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FROM=${FROM:-superflyxxi.dlinkddns.com:5000/superflyxxi/ffmpeg-vaapi:latest}
+FROM_IMAGE=${FROM_IMAGE:-superflyxxi.dlinkddns.com:5000/superflyxxi/ffmpeg-vaapi:latest}
 IMAGE=${IMAGE:-ripfile}
 
 set -ex
 cp Dockerfile .dockerfile.tmp
-sed -i "s#{FROM}#${FROM}#g" .dockerfile.tmp
+sed -i "s#{FROM_IMAGE}#${FROM_IMAGE}#g" .dockerfile.tmp
 docker build --tag ${IMAGE} -f .dockerfile.tmp .
 
 
