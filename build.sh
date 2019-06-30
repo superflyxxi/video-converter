1b8236998827236dbcebbee53107fc9e3a9865f7
SuperFlyXXI <superflyxxi@yahoo.com>
2019-06-30 15:36:10 -0400

    Print commands and fail if not successful

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 FROM=${FROM:-superflyxxi.dlinkddns.com:5000/superflyxxi/ffmpeg-vaapi:latest}
 IMAGE=${IMAGE:-ripfile}
 
+set -ex
 cp Dockerfile .dockerfile.tmp
 sed -i "s#{FROM}#${FROM}#g" .dockerfile.tmp
 docker build --tag ${IMAGE} -f .dockerfile.tmp .
