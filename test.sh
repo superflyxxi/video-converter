e8b7ff83aa360a75b98b63e36e4a02f73ab4fcd6
SuperFlyXXI <superflyxxi@yahoo.com>
2019-06-30 16:04:33 -0400

    Test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+IMAGE=${IMAGE:-ripfile}
+
+set -ex
+curl -L -o test.mpg https://alcorn.com/wp-content/downloads/test-files/AC3AlcornTest_HD.mpg
+HWACCEL=n INPUT="test.mpg" TITLE="Test" YEAR="2019" LOG=n ./ripFile.sh 
+
