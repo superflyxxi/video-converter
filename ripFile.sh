45034c413686c0e0c06494b0621b3e9478cda7f8
SuperFlyXXI <superflyxxi@yahoo.com>
2019-06-30 18:57:31 -0400

    Only do audio channel layout and quality if not copy

--- ripFile.sh
+++ ripFile.sh
@@ -50,7 +50,10 @@ AUDIO_CHANNEL_LAYOUT=${AUDIO_CHANNEL_LAYOUT:-5.1}
 AUDIO_QUALITY=${AUDIO_QUALITY:-2} # Variable Bitrate of 2 is good
 AUDIO_FORMAT=${AUDIO_FORMAT:-aac} # libfdk_aac is for aac highest quality, aac for great quality, and eac3 is Dolby Digital Ex
 # AUDIO_TRACK_ARGS="-c:a ${AUDIO_FORMAT} -q:a ${AUDIO_QUALITY} -map 0:${AUDIO_TRACK}"
-AUDIO_TRACK_ARGS="-filter:a channelmap=channel_layout=${AUDIO_CHANNEL_LAYOUT} -c:a ${AUDIO_FORMAT} -q:a ${AUDIO_QUALITY} -map 0:${AUDIO_TRACK}"
+AUDIO_TRACK_ARGS="-map 0:${AUDIO_TRACK} -c:a ${AUDIO_FORMAT}"
+if [[ "copy" != "${AUDIO_FORMAT}" ]]; then
+	AUDIO_TRACK_ARGS="-filter:a channelmap=channel_layout=${AUDIO_CHANNEL_LAYOUT} ${AUDIO_TRACK_ARGS} -q:a ${AUDIO_QUALITY}"
+fi
 
 HWACCEL=${HWACCEL:-y}
 if [[ "${HWACCEL}" == "y" ]]; then
