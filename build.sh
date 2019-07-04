9bb866d37a36329acccd1fb214a7750c53066f3a
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-04 19:03:16 -0400

    Use THIS_ docker variables

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
 
-RIPFILE_IMAGE=${RIPFILE_IMAGE:-ripfile}
+THIS_REPO=${THIS_REPO:-superflyxxi}
+THIS_REGISTRY=${THIS_REGISTRY:-build}
+THIS_IMAGE=${RIPFILE_IMAGE:-ripfile}
 LABEL=${LABEL:-latest}
 
 sed "s#{FROM_IMAGE}#${FFMPEG_DOCKER}#g" Dockerfile
-docker build --tag ${RIPFILE_IMAGE}:${LABEL} .
+docker build --tag ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${LABEL} .
 
