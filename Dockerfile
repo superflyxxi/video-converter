1690ee3b652ade02286026f924a185c7612b5e0a
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-04 17:41:42 -0400

    Simple dockerfile

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM {FROM_IMAGE}
+
--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -ex
 
+RIPFILE_IMAGE=${RIPFILE_IMAGE:-ripfile}
+LABEL=${LABEL:-latest}
+
+sed "s#{FROM_IMAGE}#${FFMPEG_DOCKER}#g" Dockerfile
+docker build --tag ${RIPFILE_IMAGE}:${LABEL} .
+
