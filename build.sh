d51e08eaa05a4cab39b70961c759c8ca5ddf47ce
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-04 19:22:27 -0400

    Cache images

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ THIS_REPO=${THIS_REPO:-superflyxxi}
 THIS_REGISTRY=${THIS_REGISTRY:-build}
 THIS_IMAGE=${RIPFILE_IMAGE:-ripfile}
 LABEL=${LABEL:-latest}
+CACHE_FROM_ARGS=${CACHE_FROM_ARGS:-"--cache-from ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:latest"}
 
 sed -i "s#{FROM_IMAGE}#${FFMPEG_DOCKER}#g" Dockerfile
 docker build --tag ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${LABEL} .
