e59931e497ee1d80fda05d3fd13e7b21e3b86885
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 16:28:27 -0400

    Add classes to represent the input file

---
+++ scripts/InputFile.php
@@ -0,0 +1,15 @@
+<?
+
+include_once "Stream.php";
+
+public class InputFile {
+
+	__construct($json) {
+	}
+
+	public $streams = NULL;
+
+}
+
+?>
+
---
+++ scripts/Stream.php
@@ -0,0 +1,11 @@
+<?
+
+public class Stream {
+
+	__construct($json) {
+	}
+
+	public $codec_type = NULL;
+	public $codec_name = NULL;
+}
+
