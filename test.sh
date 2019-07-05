8314336806195b9fa3343e23f77a06ffdc53fa58
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 12:19:19 -0400

    Remove so next test can pass

--- test.sh
+++ test.sh
@@ -12,9 +12,11 @@ fi
 
 
 docker run --rm -it -v `pwd`:/data -e INPUT=test.mpg -e HWACCEL=n -e TITLE="Test Title" -e YEAR=2019 ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${THIS_LABEL} 
-if [[ ! -f "./Test Title.ffmpeg.mkv" ]]; then
+if [[ ! -f "./Test Title (2019).ffmpeg.mkv" ]]; then
 	echo "File not created"
 	exit 1
+else
+	rm -v "./Test Title (2019).ffmpeg.mkv"
 fi
 
 #SLEEP=1s HWACCEL=n INPUT=test.mpg TITLE="Test Title" YEAR=2019 DOCKER_DAEMON=n ./ripFile.sh 
