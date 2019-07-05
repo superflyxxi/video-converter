e4352093a664fd83dedfeef16867754b84f890dc
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 12:37:02 -0400

    Verify php syntax

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -ex
 
 . common.sh
 
+php -l ripFile.php
+
 CACHE_FROM_ARGS=${CACHE_FROM_ARGS:-"--cache-from ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:latest"}
 
-#sed -i "s#{FROM_IMAGE}#${FFMPEG_DOCKER}#g" Dockerfile
 docker build ${CACHE_FROM_ARGS} --build-arg FROM_IMAGE=${FFMPEG_DOCKER} --tag ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${THIS_LABEL} .
 
