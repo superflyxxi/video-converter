709527c98aa35b2da0ddd34ce6ffcdc8ce1c0404
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 16:47:08 -0400

    Use individual types of streams

--- scripts/InputFile.php
+++ scripts/InputFile.php
@@ -5,12 +5,30 @@ include_once "Stream.php";
 public class InputFile {
 
 	__construct($json) {
+		$this->filename = $json["filename"];
 		foreach ($json["streams"] as $stream) {
-			$this->streams[$stream["index"]] = new Stream($stream);
+			$oStream = new Stream($stream);
+			$this->streams[$stream["index"]] = $oStream;
+			switch ($oStream->codec_type) {
+				case "video":
+					$this->videoStreams[$oStream->index] = $oStream;
+					break;
+				case "audio":
+					$this->audioStreams[$oStream->index] = $oStream;
+					break;
+				case "subtitle":
+					$this->subtitleStreams[$oStream->index] = $oStream;
+					break;
+				default:
+			}
 		}
 	}
 
 	public $streams = array();
+	public $subtitleStreams = array();
+	public $videoStreams = array();
+	public $audioStreams = array();
+	public $filename = NULL;
 
 }
 
