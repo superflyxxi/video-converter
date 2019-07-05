51072d5893a82b862e06d49992a617ad1127f64f
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 08:32:12 -0400

    Actually test the new image

--- test.sh
+++ test.sh
@@ -9,9 +9,12 @@ set -ex
 if [[ ! -f test.mpg ]]; then
 	curl -L -o test.mpg https://alcorn.com/wp-content/downloads/test-files/AC3AlcornTest_HD.mpg
 fi
-SLEEP=1s HWACCEL=n INPUT=test.mpg TITLE="Test Title" YEAR=2019 DOCKER_DAEMON=n ./ripFile.sh 
-docker rm test.mpg
 
-SLEEP=1s HWACCEL=n INPUT=test.mpg TITLE="Test Title" YEAR=2019 DOCKER_DAEMON=n AUDIO_FORMAT=copy ./ripFile.sh 
-docker rm test.mpg
+
+docker run --rm -it -v `pwd`:/data -e INPUT=test.mpg -e SLEEP=1s -e HWACCEL=n -e TITLE="Test Title" -e YEAR=2019 ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${THIS_LABEL} 
+#SLEEP=1s HWACCEL=n INPUT=test.mpg TITLE="Test Title" YEAR=2019 DOCKER_DAEMON=n ./ripFile.sh 
+#docker rm test.mpg
+
+#SLEEP=1s HWACCEL=n INPUT=test.mpg TITLE="Test Title" YEAR=2019 DOCKER_DAEMON=n AUDIO_FORMAT=copy ./ripFile.sh 
+#docker rm test.mpg
 
