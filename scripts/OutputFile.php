97f5dc3fb2b1a45af5e3d2af9b142a2283118d00
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 18:05:19 -0400

    Define OutputFile.php

---
+++ scripts/OutputFile.php
@@ -0,0 +1,17 @@
+<?php
+
+class OutputFile {
+	public function __construct() { }
+
+	public $title = NULL;
+	public $subtitle = NULL;
+	public $year = NULL;
+	public $searson = NULL;
+	public $episoe = NULL;
+
+	public function getOutputFile() {
+	}
+}
+
+?>
+
