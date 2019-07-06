a2ad047334db62d6fd858e813c9089d35abef904
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-06 16:36:18 -0400

    Track language on the Stream

--- php/Stream.php
+++ php/Stream.php
@@ -6,10 +6,14 @@ class Stream {
 		$this->index = $json["index"];
 		$this->codec_type = $json["codec_type"];
 		$this->codec_name = $json["codec_name"];
+		if (array_key_exists("tags", $json) && array_key_exists("language", $json["tags"])) {
+			$this->language = $json["tags"]["language"];
+		}
 	}
 
 	public $codec_type = NULL;
 	public $codec_name = NULL;
 	public $index = 0;
+	public $language = NULL;
 }
 
