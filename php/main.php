106fbea796e263cff7665de5a8ff6de0fcd0a4a1
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-06 16:14:59 -0400

    Removed commented out code

--- php/main.php
+++ php/main.php
@@ -19,22 +19,6 @@ $oOutput->season = getEnv("SEASON");
 $oOutput->year = getEnv("YEAR");
 
 $oRequest = new Request("/data/".getEnvWithDefault("INPUT", "."));
-/*$oRequest->playlist = getEnv("PLAYLIST");
-$oRequest->subtitleTrack = getEnvWithDefault("SUBTITLE_TRACK", "s?");
-$oRequest->subtitleFormat = getEnvWithDefault("SUBTITLE_FORMAT", "ass");
-
-$oRequest->audioTrack = getEnvWithDefault("AUDIO_TRACK", "a");
-$oRequest->audioFormat = getEnvWithDefault("AUDIO_FORMAT", "aac");
-$oRequest->audioQuality = getEnvWithDefault("AUDIO_QUALITY", "2");
-$oRequest->audioChannelMappingTracks = explode(" ", getEnvWithDefault("AUDIO_CHANNEL_MAPPING_TRACKS", "1"));
-
-$oRequest->deinterlace = ("true" == getEnvWithDefault("DEINTERLACE", "false"));
-
-$oRequest->videoTrack = getEnvWithDefault("VIDEO_TRACK", "v");
-$oRequest->videoFormat = getEnvWithDefault("VIDEO_FORMAT", "notcopy");
-
-$oRequest->prepareStreams();
-*/
 $otherRequests = SubtitleConvert::convert($oRequest);
 
 printf("Original Request\n");
@@ -43,28 +27,6 @@ printf("\n\nNew Additional Requests\n");
 print_r($otherRequests);
 
 $allRequests = array_merge(array($oRequest), $otherRequests);
-
-/*$finalCommand = "ffmpeg ";
-if (getEnvWithDefault("OVERWRITE_FILE", "true") == "true") {
-	$finalCommand .= "-y ";
-}
-$finalCommand .= FFmpegHelper::generateHardwareAccelArgs();
-
-// generate input args
-foreach ($allRequests as $tmpRequest) {
-	$finalCommand .= ' -i "'.$tmpRequest->oInputFile->getFileName().'" ';
-}
-
-$fileno = 0;
-foreach ($allRequests as $tmpRequest) {
-	$finalCommand .= " ".FFmpegHelper::generateArgs($fileno++, $tmpRequest);
-}
-
-$finalCommand .= FFmpegHelper::generateMetadataArgs($oOutput);
-$finalCommand .= ' -f matroska "'.$oOutput->getOutputFile().'.mkv"';
-*/
-
-
 
 $finalCommand = FFmpegHelper::generate($allRequests, $oOutput);
 
