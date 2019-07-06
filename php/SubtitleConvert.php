9df27b98e55709d0e084d12890f192612aa23ace
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-05 20:06:36 -0400

    Do subtitle conversion

---
+++ php/SubtitleConvert.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "Request.php";
+include_once "InputFile.php";
+
+class SubtitleConver {
+	public static function convert($oRequest) {
+		if ($oRequest->subtitleFormat != "copy") {
+		}
+	}
+}
+
+?>
+
