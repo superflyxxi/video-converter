b49760b04ffeaf8ef2d0e2db31c8830f4a9cfe54
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-06 16:02:20 -0400

    Actually produce an output file

--- php/OutputFile.php
+++ php/OutputFile.php
@@ -1,10 +1,12 @@
 <?php
 
+include_once "functions.php";
+
 class OutputFile {
 	
 	public function __construct() {
 		$this->envOutput = getEnv("OUTPUT");
-		$this->outputDir = getEnv("OUTPUT_DIR");
+		$this->outputDir = getEnvWithDefault("OUTPUT_DIR", "/data");
 	}
 
 	public $title = NULL;
@@ -20,7 +22,16 @@ class OutputFile {
 		if (NULL != $this->envOutput) {
 			return $this->envOutput;
 		}
-		return "wip";
+		$out = $this->outputDir."/".$this->title;
+		if (NULL != $this->year) {
+			$out .= " (".$this->year.")";
+		} else if (NULL != $this->season) {
+			$out .= " - s".$this->season."e".$this->episode;
+		}
+		if (NULL != $this->subtitle) {
+			$out .= " ".$this->subtitle;
+		}
+		return $out;
 	}
 }
 
