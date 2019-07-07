e1d0f7ebd3acb9ec2b9f3ef50ef5c222c825729b
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-06 21:51:07 -0400

    Test tv show

---
+++ tests/test_03_tv_show.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once "common.php";
+
+$command = 'docker run --rm -t -v `pwd`:/data -e INPUT=test.mpg -e TITLE="Test tv show" -e SEASON=01 -e EPISODE=23 -e SUBTITLE="The One Where Things" '.$image;
+printf("executing: %s\n", $command);
+exec($command, $output, $return);
+test("ffmpeg code", 0, $return);
+
+$probe = probe("/data/Test tv show - s01e23 - The One Where Things.ffmpeg.mkv");
+$probe = json_decode($probe, true);
+
+test("Stream 0", "video", $probe["streams"][0]["codec_type"]);
+test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"]);
+test("Stream 1", "audio", $probe["streams"][1]["codec_type"]);
+test("Stream 1 codec", "aac", $probe["streams"][1]["codec_name"]);
+test("Metadata Title", "Test tv show", $probe["format"]["tags"]["title"]);
+test("Metadata SEASON", "01", $probe["format"]["tags"]["SEASON"]);
+test("Metadata EPISODE", "23", $probe["format"]["tags"]["EPISODE"]);
+test("Metadata SUBTITLE", "The One Where Things", $probe["format"]["tags"]["SUBTITLE"]);
+
+?>
+
