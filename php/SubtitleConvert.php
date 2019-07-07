f34fb02ee950edfc33ee604be0957c2d535be200
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-07 16:03:47 -0400

    use command FFmpeg class for using ffmpeg

--- php/SubtitleConvert.php
+++ php/SubtitleConvert.php
@@ -16,46 +16,39 @@ class SubtitleConvert {
 				$dvdFile = NULL;
 				if ("hdmv_pgs_subtitle" == $codecName) {
 					// convert to dvd
-					//$dvdFile = getEnvWithDefault("TMP_DIR", "/tmp")."/".$filename.'-'.$subtitle->index.'.sub';
-					$pgsFile = $dir."/".$filename.'-'.$index.'.sup';
-					if (!file_exists($pgsFile)) {
-						$command = 'ffmpeg -y -i "'.$filename.'" -map 0:'.$index.' -c copy "'.$pgsFile.'"';
-						printf("Extract pgs command: %s\n", $command);
-						exec($command, $out, $return);
-						if ($return != 0) {
-							printf("pgs extract failed: %s\n", $return);
-							exit($return);
-						}
-					}
+					$pgsFile = new OutputFile($dir."/".$filename.'-'.$index.'.sup');
+					$pgsRequest = new Request($filename);
+					$pgsRequest->subtitleTrack = $index;
+					$pgsRequest->subtitleFormat = "copy";
+					$pgsRequest->prepareStreams();
+					printf("Generating PGS sup file for index %s of file %s.\n", $index, $filename);
+					FFmpegHelper::execute(array($pgsRequest), $pgsFile);
+
 					$dvdFile = $dir."/".$filename.'-'.$index;
-					if (!file_exists($dvdFile.".sub")) {
-						$command = 'java -jar /home/ripvideo/BDSup2Sub.jar -o "'.$dvdFile.'.sub" "'.$pgsFile.'"';
-						printf("Convert pgs to dvd command: %s\n", $command);
-						exec($command, $out, $return);
-						if ($return != 0) {
-						    printf("sub convertion failed: %s\n", $return);
-						    exit($return);
-						}
+					$command = 'java -jar /home/ripvideo/BDSup2Sub.jar -o "'.$dvdFile.'.sub" "'.$pgsFile->getFileName().'"';
+					printf("Convert pgs to dvd command: %s\n", $command);
+					exec($command, $out, $return);
+					if ($return != 0) {
+					    printf("sub convertion failed: %s\n", $return);
+					    exit($return);
 					}
 				} else if ("vobsub" == $codeName) {
 					// extract vobsub
 					$dvdFile = $dir."/".$filename.'-'.$index;
-					if (!file_exists($dvdFile.".sub")) {
-						$command = 'ffmpeg -y -i "'.$filename.'" -map 0:'.$index.' -c copy "'.$dvdFile.'.sub"';
-						printf("Extract vobsub command: %s\n", $command);
-						exec($command, $out, $return);
-						if ($return != 0) {
-						    printf("vobsub extraction failed: %s\n", $return);
-						    exit($return);
-						}
-					}
+					$dvdOutputFile = new OutputFile($dvdFile.".sub");
+					$dvdRequest = new Request($filename);
+					$dvdRequest->subtitleTrack = $index;
+					$dvdRequest->subtitleFormat = "copy";
+					$dvdRequest->prepareStreams();
+					printf("Generating DVD sub file for index %s of file %s.\n", $index, $filename);
+					FFmpegHelper::execute(array($dvdRequest), $dvdOutputFile);
 				}
 										
 				// convert to srt
 				if (NULL != $dvdFile) {
 					if (!file_exists($dvdFile.".srt")) {
 	                                        $command = 'vobsub2srt "'.$dvdFile.'"';
-        	                                printf("Convert vobsub to srt command: %s\n", $command);
+        	                                printf("Convert DVD sub using command: %s\n", $command);
                 	                        exec($command, $out, $return);
                         	                if ($return != 0) {
                                 	            printf("vobsub to srt conversion failed: %s\n", $return);
@@ -65,6 +58,7 @@ class SubtitleConvert {
 					$oNewRequest = new Request($dvdFile.".srt");
 					$oNewRequest->subtitleTrack = 0;
 					$oNewRequest->subtitleFormat = $oRequest->subtitleFormat;
+					$oNewRequest->prepareStreams();
 					$arrAdditionalRequests[] = $oNewRequest;
 					$oRequest->oInputFile->removeSubtitleStream($index);
                                 }
