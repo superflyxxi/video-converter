819203149f3bc9210b1eb7138cd68584faf4ca06
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-07 11:05:52 -0400

    Fixed basic stuff, but need to caputre output of JSON

--- php/NormalizeAudio.php
+++ php/NormalizeAudio.php
@@ -10,37 +10,41 @@ class NormalizeAudio {
         $arrAdditionalRequests = array();
         if ("copy" != $oRequest->audioFormat) {
             $dir = getEnvWithDefault("TMP_DIR", "/tmp");
-            foreach ($oRequest->$normalizeAudioTracks as $index) {
-                $command = 'ffmpeg -i "'.$oRequest->oInputFile->getFileName()
-                        .'" -map 0:'.$index.' -filter:a loudnorm=print_format=json -f null -';
-                printf("Measuring %s with command: %s\n", $index, $command);
-                exec($command, $out, $return);
-                print_r($out);
-                if ($return != 0) {
-                    printf("Normalizing failed: %s\n", $return);
-                    exit($return);
-                }
-                $json = json_encode(implode($out), true);
-                print_r($json);
+            foreach ($oRequest->normalizeAudioTracks as $index) {
+		if (is_numeric($index)) {
+	                $command = 'ffmpeg -hide_banner -i "'.$oRequest->oInputFile->getFileName()
+        	                .'" -map 0:'.$index.' -filter:a loudnorm=print_format=json -f null - ';
+	                printf("Measuring %s with command: %s\n", $index, $command);
+        	        exec($command, $out, $return);
+                	printf("Output: \n");
+			print_r($out);
+        	        if ($return != 0) {
+                	    printf("Normalizing failed: %s\n", $return);
+	                    exit($return);
+	                }
+        	        $json = json_decode(implode($out), true);
+			printf("JSON:\n");
+	                print_r($json);
+        
+	                $stream = $oRequest->oInputFile->getAudioStreams()[$index];
                 
-                $stream = $oRequest->oInputFile->getAudioStreams()[$index];
-                
-                $tmpFile = $dir.$oRequest->oInputFile->getInputFileName().'-'.$index.'-norm.mkv';
-                $command = 'ffmpeg -i "'.$oRequest->oInputFile->getFileName()
-                        .'" -y -map 0:'.$index
-                        .' -filter:a loudnorm=measured_I='.$json["measured_I"]
-                        .':measured_TP='.$json["measured_TP"]
-                        .':measured_LRA='.$json["measured_LRA"]
-                        .':measured_thresh='.$json["measured_thresh"] 
-                        .' channelmap=channel_layout='.$stream->channel_layout
-                        .' -c:a '.$oRequest->audioFormat
-                        .' -q:a '.$oRequest->audioQuality
-                        .' -metadata:s:a:0 "title=Normalized '.$stream->language.' '.$stream->channel_layout.'"'
-                        .' -f matroska "'.$tmpFile.'"';
-                $oNewRequest = new Request($tmpFile);
-                $oNewRequest->audioTrack = 0;
-                $oNewRequest->audioFormat = "copy";
-                $arrAdditionalRequests[] = $oNewRequest;
+        	        $tmpFile = $dir.$oRequest->oInputFile->getFileName().'-'.$index.'-norm.mkv';
+                	$command = 'ffmpeg -i "'.$oRequest->oInputFile->getFileName()
+	                        .'" -y -map 0:'.$index
+        	                .' -filter:a loudnorm=measured_I='.$json["input_i"]
+                	        .':measured_TP='.$json["input_tp"]
+                        	.':measured_LRA='.$json["input_lra"]
+	                        .':measured_thresh='.$json["input_thresh"] 
+				.(NULL != $stream->channel_layout ? ' channelmap=channel_layout='.$stream->channel_layout : ' ')
+                	        .' -c:a '.$oRequest->audioFormat
+                        	.' -q:a '.$oRequest->audioQuality
+	                        .' -metadata:s:a:0 "title=Normalized '.$stream->language.' '.$stream->channel_layout.'"'
+        	                .' -f matroska "'.$tmpFile.'"';
+                	$oNewRequest = new Request($tmpFile);
+	                $oNewRequest->audioTrack = 0;
+        	        $oNewRequest->audioFormat = "copy";
+                	$arrAdditionalRequests[] = $oNewRequest;
+		}
             }
         }
         return $arrAdditionalRequests;
