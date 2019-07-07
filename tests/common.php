68d7bff225fcc8081c775fd649b85a2213da1956
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-06 20:13:53 -0400

    Added basic test

--- tests/common.php
+++ tests/common.php
@@ -6,9 +6,23 @@ $image = getEnv("THIS_REGISTRY").'/'.getEnv("THIS_REPO").'/'.getEnv("THIS_IMAGE"
 
 function test($message, $expected, $actual) {
 	if ($expected !== $actual) {
-		printf("%s. Expected=%s, but got=%s", $message, $expected, $actual);
+		printf("FAIL: %s. Expected='%s', but got='%s'\n", $message, $expected, $actual);
 		exit(1);
 	}
+	printf("PASS: %s. Got expected='%s'\n", $message, $expected, $actual);
+}
+
+function probe($file) {
+	global $user;
+	global $image;
+	$command = 'docker run --rm -it --user='.$user.' -v `pwd`:/data --entrypoint ffprobe '.$image.' -v quiet -print_format json -show_format -show_streams "'.$file.'"';
+#	$command = 'ffprobe -v quiet -print_format json -show_format -show_streams "'.$file.'"';
+	printf("Probing '%s'\n", $file);
+	exec($command, $out, $ret);
+	if ($ret == 0) {
+		return implode($out);
+	}
+	return NULL;
 }
 
 ?>
--- tests/test_01_basic.php
+++ tests/test_01_basic.php
@@ -2,11 +2,20 @@
 
 include_once "common.php";
 
-exec('docker run --rm -it --user='.$user.' -v `pwd`:/data -e INPUT=test.mpg -e TITLE="Test default" -e YEAR=2019 '.$image, $output, $return);
+$command = 'docker run --rm -it --user='.$user.' -v `pwd`:/data -e INPUT=test.mpg -e TITLE="Test default" -e YEAR=2019 '.$image;
+printf("executing: %s\n", $command);
+exec($command, $output, $return);
 test("ffmpeg code", 0, $return);
 
-exec('docker run --rm -it --user='.$user.' -v `pwd`:/data --entrypoint ffprobe '.$image.' -i "/data/Test default (2019).ffmpeg.mkv"', $output, $return);
-test("ffprobe", 0, $return);
+$probe = probe("/data/Test default (2019).ffmpeg.mkv");
+$probe = json_decode($probe, true);
+
+test("Stream 0", "video", $probe["streams"][0]["codec_type"]);
+test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"]);
+test("Stream 1", "audio", $probe["streams"][1]["codec_type"]);
+test("Stream 1 codec", "aac", $probe["streams"][1]["codec_name"]);
+test("Metadata Title", "Test default", $probe["format"]["tags"]["title"]);
+test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"]);
 
 ?>
 
