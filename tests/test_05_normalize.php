202d442f14023fb682e01a94049138aa3729e99a
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-07 21:47:00 -0400

    Don't print so much

--- tests/test_05_normalize.php
+++ tests/test_05_normalize.php
@@ -6,19 +6,17 @@ $command = 'docker run --rm -t -v `pwd`:/data -e INPUT=test.mpg -e TITLE="Test N
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 
-print_r($output);
 test("ffmpeg code", 0, $return);
 
 $probe = probe("/data/Test Normalize Track 1 (2019).ffmpeg.mkv");
 $probe = json_decode($probe, true);
-
-print_r($probe);
 
 test("Stream 0", "video", $probe["streams"][0]["codec_type"]);
 test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"]);
 test("Stream 1", "audio", $probe["streams"][1]["codec_type"]);
 test("Stream 1 codec", "aac", $probe["streams"][1]["codec_name"]);
 test("Stream 1 channels", 6, $probe["streams"][1]["channels"]);
+test("Stream 1 channel_layout", "5.1", $probe["streams"][1]["channel_layout"]);
 test("Stream 2", "audio", $probe["streams"][2]["codec_type"]);
 test("Stream 2 codec", "aac", $probe["streams"][2]["codec_name"]);
 test("Stream 2 channel_layout", "5.1", $probe["streams"][2]["channel_layout"]);
@@ -29,9 +27,6 @@ test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"]);
 test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]));
 test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]));
 test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]));
-
-// failnig test
-test("Stream 2 channel_layout", "5.1", $probe["streams"][1]["channel_layout"]);
 
 ?>
 
