4a67570c0b52fcd98c2d70b8e8132a6c110e2ce8
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-10 07:49:10 -0400

    Dummy test file for bluray iso

---
+++ tests/test_06_bluray_iso.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once "common.php";
+if (!file_exists(getEnv("TMP_DIR")."/test_bluray.iso")) {
+	file_put_contents(getEnv("TMP_DIR")."/test_bluray.iso", fopen("http://superflyxxi.dlinkddns.com/samples/Some_Trailers.iso", 'r'));
+}
+
+$command = 'docker run --rm -t -v `pwd`:/data -e INPUT=test_bluray.iso -e TITLE="Test BluRay ISO" -e YEAR=2019 '.$image;
+printf("executing: %s\n", $command);
+exec($command, $output, $return);
+
+test("ffmpeg code", 0, $return, $output);
+
+$probe = probe("/data/Test BluRay ISO (2019).ffmpeg.mkv");
+$probe = json_decode($probe, true);
+
+test("Stream 0", "video", $probe["streams"][0]["codec_type"]);
+test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"]);
+test("Stream 1", "audio", $probe["streams"][1]["codec_type"]);
+test("Stream 1 codec", "aac", $probe["streams"][1]["codec_name"]);
+test("Stream 1 channel_layout", "5.1", $probe["streams"][1]["channel_layout"]);
+test("Stream 1 channels", 6, $probe["streams"][1]["channels"]);
+test("Metadata Title", "Test default", $probe["format"]["tags"]["title"]);
+test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"]);
+test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]));
+test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]));
+test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]));
+
+?>
+
--- travis/test.sh
+++ travis/test.sh
@@ -7,9 +7,10 @@ set -ex
 . common.sh
 
 cd ../tests/
+export TMP_DIR=$(mktemp -d)
 
-if [[ ! -f test.mpg ]]; then
-	curl -L -o test.mpg https://alcorn.com/wp-content/downloads/test-files/AC3AlcornTest_HD.mpg
+if [[ ! -f "${TMP_DIR}/test.mpg" ]]; then
+	curl -L -o "${TMP_DIR}/test.mpg" "https://alcorn.com/wp-content/downloads/test-files/AC3AlcornTest_HD.mpg"
 fi
 
 find . -name 'test*.php' | xargs -L1 php
