a7ca0c27b83786c1fa18956632b77a4c229d48b0
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-10 22:02:34 -0400

    Enable logging for tests

--- travis/test.sh
+++ travis/test.sh
@@ -8,6 +8,7 @@ set -ex
 
 cd ../tests/
 export TMP_DIR=$(mktemp -d)
+export LOG_LEVEL=VERBOSE
 
 if [[ ! -f "${TMP_DIR}/test.mpg" ]]; then
 	curl -L -o "${TMP_DIR}/test.mpg" "https://alcorn.com/wp-content/downloads/test-files/AC3AlcornTest_HD.mpg"
