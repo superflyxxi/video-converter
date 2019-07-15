4c74fd2cbe42875e49ba0a41a802fdc5b4561560
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-14 21:13:50 -0400

    If normalizing anything, transform each audio track and if req, normalize it

--- php/NormalizeAudio.php
+++ php/NormalizeAudio.php
@@ -9,33 +9,36 @@ class NormalizeAudio {
     
     public static function normalize($oRequest) {
         $arrAdditionalRequests = array();
-        if ("copy" != $oRequest->audioFormat) {
+        if ("copy" != $oRequest->audioFormat && count($oRequest->normalizeAudioTracks)) {
+	    // only do this there are tracks to normalize
             $dir = getEnvWithDefault("TMP_DIR", "/tmp");
-            foreach ($oRequest->normalizeAudioTracks as $index) {
-		if (is_numeric($index)) {
+	    // any track that is not needed, just copy it to its own file
+	    foreach ($oRequest->getAudioStreams() as $reqIndex => $reqStream) {
+	        $stream = $reqStream;
+
+		// copy original always and add to list of additional requests
+		$tmpRequest = new Request($oRequest->oInputFile->getFileName());
+		$tmpRequest->audioTrack = $reqIndex;
+		$tmpRequest->audioFormat = $oRequest->audioFormat;
+		$tmpRequest->audioQuality = $oRequest->audioQuality;
+		$tmpRequest->audioChannelMapping = $oRequest->audioChannelMapping;
+		$tmpRequest->prepareStreams();
+		if ($oRequest->oInputFile->getPrefix() != NULL) {
+			$origOutFile = new OutputFile($dir.realpath($oRequest->oInputFile->getFileName()).'/dir-'.$index.'-orig.mkv');
+		} else {
+			$origOutFile = new OutputFile($dir.$oRequest->oInputFile->getFileName().'-'.$index.'-orig.mkv');
+		}
+		FFmpegHelper::execute(array($tmpRequest), $origOutFile);
+                $oNewRequest = new Request($origOutFile->getFileName());
+	        $oNewRequest->audioTrack = 0;
+		$oNewRequest->audioFormat= "copy";
+		$oNewRequest->prepareStreams();
+                $arrAdditionalRequests[] = $oNewRequest;
+		$oRequest->oInputFile->removeAudioStream($reqIndex);
+
+		if (in_array($reqIndex, $oRequest->normalizeAudioTracks)) {
+			// if the track is to be normalized, now let's normalize it and put it in
 			Logger::info("Normalizing track {}:{}", array($oRequest->oInputFile->getFileName(), $index));
-	                $stream = $oRequest->oInputFile->getAudioStreams()[$index];
-
-			$tmpRequest = new Request($oRequest->oInputFile->getFileName());
-			$tmpRequest->audioTrack = $index;
-			$tmpRequest->audioFormat = $oRequest->audioFormat;
-			$tmpRequest->audioQuality = $oRequest->audioQuality;
-			$tmpRequest->audioChannelMapping = $oRequest->audioChannelMapping;
-			$tmpRequest->prepareStreams();
-			if ($oRequest->oInputFile->getPrefix() != NULL) {
-				$origOutFile = new OutputFile($dir.realpath($oRequest->oInputFile->getFileName()).'/dir-'.$index.'-orig.mkv');
-			} else {
-				$origOutFile = new OutputFile($dir.$oRequest->oInputFile->getFileName().'-'.$index.'-orig.mkv');
-			}
-			FFmpegHelper::execute(array($tmpRequest), $origOutFile);
-			
-                	$oNewRequest = new Request($origOutFile->getFileName());
-	                $oNewRequest->audioTrack = 0;
-			$oNewRequest->audioFormat= "copy";
-			$oNewRequest->prepareStreams();
-                	$arrAdditionalRequests[] = $oNewRequest;
-			$oRequest->oInputFile->removeAudioStream($index);
-
 	                $command = 'ffmpeg -hide_banner -i "'.$origOutFile->getFileName()
         	                .'" -map 0 -filter:a loudnorm=print_format=json -f null - 2>&1';
 	                Logger::info("Measuring {}:{} with command: {}", array($oRequest->oInputFile->getFileName(), $index, $command));
@@ -77,7 +80,7 @@ class NormalizeAudio {
         	        $oNewRequest->audioFormat = "copy";
                 	$arrAdditionalRequests[] = $oNewRequest;
 		}
-            }
+	    }
         }
         return $arrAdditionalRequests;
     }
