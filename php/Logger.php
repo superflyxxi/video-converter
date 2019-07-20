da080db841e0c169a475b25d42da62ff5bb2253c
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-20 13:49:20 -0400

    Cleaned up all src php files

--- php/Logger.php
+++ php/Logger.php
@@ -1,71 +1,81 @@
 <?php
 
-class Logger {
-	
-	const VERBOSE = 0;
-	const DEBUG = 1;
-	const WARN = 2;
-	const ERROR = 3;
-	const INFO = 4;
-	
-	const dateformat = "c";
+class Logger
+{
 
-	private static $loglevel = -1;
-	
-	public static function info($msg, array $args = array()) {
-		self::log(self::INFO, $msg, $args);
-	}
+    const VERBOSE = 0;
 
-	public static function warn($msg, array $args = array()) {
-		self::log(self::WARN, $msg, $args);
-	}
+    const DEBUG = 1;
 
-	public static function error($msg, array $args = array()) {
-		self::log(self::ERROR, $msg, $args);
-	}
+    const WARN = 2;
 
-	public static function verbose($msg, array $args = array()) {
-		self::log(self::VERBOSE, $msg, $args);
-	}
+    const ERROR = 3;
 
-	public static function debug($msg, array $args = array()) {
-		self::log(self::DEBUG, $msg, $args);
-	}
+    const INFO = 4;
 
-	public static function log($reqlevel, $msg, array $args) {
-		self::init();
-		if (self::$loglevel >= $reqlevel) {
-			$str = print_r($msg, true);
-			foreach ($args as $arg) {
-				$str = preg_replace("/{}/", print_r($arg, true), $str, 1);
-			}
-			printf("%s::%s::%s\n", date(self::dateformat), $reqlevel, $str);
-		}
-	}
+    const dateformat = "c";
 
-	private static function init() {
-		if (self::$loglevel == -1) {
-			date_default_timezone_set("UTC");
-			switch (getEnv("LOG_LEVEL")) {
-				case "WARN":
-					self::$loglevel = self::WARN;
-					break;
-				case "ERROR":
-					self::$loglevel = self::ERROR;
-					break;
-				case "VERBOSE":
-					self::$loglevel = self::VERBOSE;
-					break;
-				case "DEBUG":
-					self::$loglevel = self::DEBUG;
-					break;
-				default:
-					self::$loglevel = self::INFO;
-			}
-		}
-	}
+    private static $loglevel = - 1;
 
+    public static function info($msg, array $args = array())
+    {
+        self::log(self::INFO, $msg, $args);
+    }
+
+    public static function warn($msg, array $args = array())
+    {
+        self::log(self::WARN, $msg, $args);
+    }
+
+    public static function error($msg, array $args = array())
+    {
+        self::log(self::ERROR, $msg, $args);
+    }
+
+    public static function verbose($msg, array $args = array())
+    {
+        self::log(self::VERBOSE, $msg, $args);
+    }
+
+    public static function debug($msg, array $args = array())
+    {
+        self::log(self::DEBUG, $msg, $args);
+    }
+
+    public static function log($reqlevel, $msg, array $args)
+    {
+        self::init();
+        if (self::$loglevel >= $reqlevel) {
+            $str = print_r($msg, true);
+            foreach ($args as $arg) {
+                $str = preg_replace("/{}/", print_r($arg, true), $str, 1);
+            }
+            printf("%s::%s::%s\n", date(self::dateformat), $reqlevel, $str);
+        }
+    }
+
+    private static function init()
+    {
+        if (self::$loglevel == - 1) {
+            date_default_timezone_set("UTC");
+            switch (getEnv("LOG_LEVEL")) {
+                case "WARN":
+                    self::$loglevel = self::WARN;
+                    break;
+                case "ERROR":
+                    self::$loglevel = self::ERROR;
+                    break;
+                case "VERBOSE":
+                    self::$loglevel = self::VERBOSE;
+                    break;
+                case "DEBUG":
+                    self::$loglevel = self::DEBUG;
+                    break;
+                default:
+                    self::$loglevel = self::INFO;
+            }
+        }
+    }
 }
 
 ?>
-
--- php/NormalizeAudio.php
+++ php/NormalizeAudio.php
@@ -1,84 +1,89 @@
 <?php
-
 include_once "Logger.php";
 include_once "functions.php";
 include_once "Request.php";
 include_once "Stream.php";
 
-class NormalizeAudio {
-    
-    public static function normalize($oRequest) {
+class NormalizeAudio
+{
+
+    public static function normalize($oRequest)
+    {
         $arrAdditionalRequests = array();
         if ("copy" != $oRequest->audioFormat && count($oRequest->normalizeAudioTracks)) {
-	    // only do this there are tracks to normalize
+            // only do this there are tracks to normalize
             $dir = getEnvWithDefault("TMP_DIR", "/tmp");
-	    // any track that is not needed, just copy it to its own file
-	    foreach ($oRequest->oInputFile->getAudioStreams() as $index => $stream) {
-		// copy original always and add to list of additional requests
-		$tmpRequest = new Request($oRequest->oInputFile->getFileName());
-		$tmpRequest->audioTrack = $index;
-		$tmpRequest->audioFormat = $oRequest->audioFormat;
-		$tmpRequest->audioQuality = $oRequest->audioQuality;
-		$tmpRequest->audioChannelMapping = $oRequest->audioChannelMapping;
-		$tmpRequest->prepareStreams();
-		if ($oRequest->oInputFile->getPrefix() != NULL) {
-			$origOutFile = new OutputFile($dir.realpath($oRequest->oInputFile->getFileName()).'/dir-'.$index.'-orig.mkv');
-		} else {
-			$origOutFile = new OutputFile($dir.$oRequest->oInputFile->getFileName().'-'.$index.'-orig.mkv');
-		}
-		FFmpegHelper::execute(array($tmpRequest), $origOutFile);
+            // any track that is not needed, just copy it to its own file
+            foreach ($oRequest->oInputFile->getAudioStreams() as $index => $stream) {
+                // copy original always and add to list of additional requests
+                $tmpRequest = new Request($oRequest->oInputFile->getFileName());
+                $tmpRequest->audioTrack = $index;
+                $tmpRequest->audioFormat = $oRequest->audioFormat;
+                $tmpRequest->audioQuality = $oRequest->audioQuality;
+                $tmpRequest->audioChannelMapping = $oRequest->audioChannelMapping;
+                $tmpRequest->prepareStreams();
+                if ($oRequest->oInputFile->getPrefix() != NULL) {
+                    $origOutFile = new OutputFile($dir . realpath($oRequest->oInputFile->getFileName()) . '/dir-' . $index . '-orig.mkv');
+                } else {
+                    $origOutFile = new OutputFile($dir . $oRequest->oInputFile->getFileName() . '-' . $index . '-orig.mkv');
+                }
+                FFmpegHelper::execute(array(
+                    $tmpRequest
+                ), $origOutFile);
                 $oNewRequest = new Request($origOutFile->getFileName());
-	        $oNewRequest->audioTrack = 0;
-		$oNewRequest->audioFormat= "copy";
-		$oNewRequest->prepareStreams();
+                $oNewRequest->audioTrack = 0;
+                $oNewRequest->audioFormat = "copy";
+                $oNewRequest->prepareStreams();
                 $arrAdditionalRequests[] = $oNewRequest;
-		$oRequest->oInputFile->removeAudioStream($index);
-
-		if (in_array($index, $oRequest->normalizeAudioTracks)) {
-			// if the track is to be normalized, now let's normalize it and put it in
-			Logger::info("Normalizing track {}:{}", array($oRequest->oInputFile->getFileName(), $index));
-	                $command = 'ffmpeg -hide_banner -i "'.$origOutFile->getFileName()
-        	                .'" -map 0 -filter:a loudnorm=print_format=json -f null - 2>&1';
-	                Logger::info("Measuring {}:{} with command: {}", array($oRequest->oInputFile->getFileName(), $index, $command));
-        	        exec($command, $out, $return);
-			Logger::verbose($out);
-        	        if ($return != 0) {
-                	    Logger::error("Normalizing failed: {}", array($return));
-	                    exit($return);
-	                }
-        	        $out = implode(array_slice($out, -12));
-        	        $json = json_decode($out, true);
+                $oRequest->oInputFile->removeAudioStream($index);
                 
-        	        $normFile = $dir.$oRequest->oInputFile->getFileName().'-'.$index.'-norm.mkv';
-			$normChannelMap = array_key_exists($index, $oRequest->audioChannelMapping) 
-				? $oRequest->audioChannelMapping[$index] 
-				: $stream->channel_layout;
-
-			$normChannelMap = preg_replace("/\(.+\)/", '', $normChannelMap);
-                	$command = 'ffmpeg -i "'.$origOutFile->getFileName().'" -y -map 0'
-        	                .' -filter:a "loudnorm=measured_I='.$json["input_i"]
-                	        .':measured_TP='.$json["input_tp"]
-                        	.':measured_LRA='.$json["input_lra"]
-	                        .':measured_thresh='.$json["input_thresh"] 
-				.(NULL != $normChannelMap ? ',channelmap=channel_layout='.$normChannelMap : '')
-				.'" '
-                	        .' -c:a '.$oRequest->audioFormat
-                        	.' -q:a '.$oRequest->audioQuality
-	                        .' -metadata:s:a:0 "title=Normalized '.$stream->language.' '.$normChannelMap.'"'
-        	                .' -f matroska "'.$normFile.'" 2>&1';
-
-	                Logger::info("Normalizing {}:{} with command: {}", array($oRequest->oInputFile->getFileName(), $index, $command));
-        	        passthru($command, $return);
-        	        if ($return != 0) {
-                	    Logger::error("Normalizing failed: {}", array($return));
-	                    exit($return);
-	                }
-                	$oNewRequest = new Request($normFile);
-	                $oNewRequest->audioTrack = 0;
-        	        $oNewRequest->audioFormat = "copy";
-                	$arrAdditionalRequests[] = $oNewRequest;
-		}
-	    }
+                if (in_array($index, $oRequest->normalizeAudioTracks)) {
+                    // if the track is to be normalized, now let's normalize it and put it in
+                    Logger::info("Normalizing track {}:{}", array(
+                        $oRequest->oInputFile->getFileName(),
+                        $index
+                    ));
+                    $command = 'ffmpeg -hide_banner -i "' . $origOutFile->getFileName() . '" -map 0 -filter:a loudnorm=print_format=json -f null - 2>&1';
+                    Logger::info("Measuring {}:{} with command: {}", array(
+                        $oRequest->oInputFile->getFileName(),
+                        $index,
+                        $command
+                    ));
+                    exec($command, $out, $return);
+                    Logger::verbose($out);
+                    if ($return != 0) {
+                        Logger::error("Normalizing failed: {}", array(
+                            $return
+                        ));
+                        exit($return);
+                    }
+                    $out = implode(array_slice($out, - 12));
+                    $json = json_decode($out, true);
+                    
+                    $normFile = $dir . $oRequest->oInputFile->getFileName() . '-' . $index . '-norm.mkv';
+                    $normChannelMap = array_key_exists($index, $oRequest->audioChannelMapping) ? $oRequest->audioChannelMapping[$index] : $stream->channel_layout;
+                    
+                    $normChannelMap = preg_replace("/\(.+\)/", '', $normChannelMap);
+                    $command = 'ffmpeg -i "' . $origOutFile->getFileName() . '" -y -map 0' . ' -filter:a "loudnorm=measured_I=' . $json["input_i"] . ':measured_TP=' . $json["input_tp"] . ':measured_LRA=' . $json["input_lra"] . ':measured_thresh=' . $json["input_thresh"] . (NULL != $normChannelMap ? ',channelmap=channel_layout=' . $normChannelMap : '') . '" ' . ' -c:a ' . $oRequest->audioFormat . ' -q:a ' . $oRequest->audioQuality . ' -metadata:s:a:0 "title=Normalized ' . $stream->language . ' ' . $normChannelMap . '"' . ' -f matroska "' . $normFile . '" 2>&1';
+                    
+                    Logger::info("Normalizing {}:{} with command: {}", array(
+                        $oRequest->oInputFile->getFileName(),
+                        $index,
+                        $command
+                    ));
+                    passthru($command, $return);
+                    if ($return != 0) {
+                        Logger::error("Normalizing failed: {}", array(
+                            $return
+                        ));
+                        exit($return);
+                    }
+                    $oNewRequest = new Request($normFile);
+                    $oNewRequest->audioTrack = 0;
+                    $oNewRequest->audioFormat = "copy";
+                    $arrAdditionalRequests[] = $oNewRequest;
+                }
+            }
         }
         return $arrAdditionalRequests;
     }
