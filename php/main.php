48e4ce6f29d3a25fe5303d12d752f3474eb229de
SuperFlyXXI <superflyxxi@yahoo.com>
2019-07-20 13:46:25 -0400

    Cleaned up format

--- php/main.php
+++ php/main.php
@@ -1,6 +1,5 @@
 #!/bin/php
 <?php
-
 include_once "Logger.php";
 include_once "Request.php";
 include_once "OutputFile.php";
@@ -9,9 +8,9 @@ include_once "SubtitleConvert.php";
 include_once "NormalizeAudio.php";
 include_once "FFmpegHelper.php";
 
-if (!getEnv("TITLE")) {
-	Logger::error("Missing TITLE variable");
-	exit(1);
+if (! getEnv("TITLE")) {
+    Logger::error("Missing TITLE variable");
+    exit(1);
 }
 Logger::info("Starting conversion");
 $oOutput = new OutputFile();
@@ -21,15 +20,16 @@ $oOutput->season = getEnv("SEASON");
 $oOutput->episode = getEnv("EPISODE");
 $oOutput->year = getEnv("YEAR");
 
-$oRequest = Request::newInstanceFromEnv("/data/".getEnvWithDefault("INPUT", "."));
+$oRequest = Request::newInstanceFromEnv("/data/" . getEnvWithDefault("INPUT", "."));
 $allRequests[] = $oRequest;
 $allRequests = array_merge($allRequests, NormalizeAudio::normalize($oRequest));
 $allRequests = array_merge($allRequests, SubtitleConvert::convert($oRequest));
 
 $returnValue = FFmpegHelper::execute($allRequests, $oOutput);
 
-Logger::info("Completed conversion with {} as a return value.", array($returnValue));
+Logger::info("Completed conversion with {} as a return value.", array(
+    $returnValue
+));
 exit($returnValue);
 
 ?>
-
