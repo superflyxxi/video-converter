f3384deaba18144bfcf7efcabacccc7994597d36
SuperFlyXXI <superflyxxi@yahoo.com>
2019-08-10 15:26:06 -0400

    Don't label the image fully until pushing

--- travis/build.sh
+++ travis/build.sh
@@ -9,5 +9,5 @@ find . -name *.php | xargs -L1 php -l
 
 CACHE_FROM_ARGS=${CACHE_FROM_ARGS:-"--cache-from ${CACHE_FROM_IMAGE}"}
 
-docker build ${CACHE_FROM_ARGS} --build-arg FROM_IMAGE=${FROM_IMAGE} --tag ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${THIS_LABEL} -f Dockerfile.${FFMPEG_DOCKER_LABEL} .
+docker build ${CACHE_FROM_ARGS} --build-arg FROM_IMAGE=${FROM_IMAGE} --tag rip-video:build-${THIS_LABEL} -f Dockerfile.${FFMPEG_DOCKER_LABEL} .
 
--- travis/push.sh
+++ travis/push.sh
@@ -3,5 +3,6 @@ set -ex
 
 . common.sh
 
+docker tag rip-video:build-${THIS_LABEL} ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${THIS_LABEL}
 docker push ${THIS_REGISTRY}/${THIS_REPO}/${THIS_IMAGE}:${THIS_LABEL}
 
