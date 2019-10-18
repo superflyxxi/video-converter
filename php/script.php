812f865a2fa4822235c13e794853be5792fb5e9d
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-18 09:55:56 -0400

    Introduce error handler to ensure script fails on any bug

--- php/script.php
+++ php/script.php
@@ -1,6 +1,12 @@
 <?php
 include_once "ConvertFile.php";
 
+function error_handler(int $errno, string $errstr, string $errfile = NULL, int $errline = 0, array $errcontext = NULL)
+{
+    throw new ErrorException("$errstr in $errfile", $errno);
+}
+set_error_handler('errro_handler');
+
 if (NULL == getEnv("TITLE")) {
     Logger::error("TITLE env variable missing");
     exit(1);
@@ -15,9 +21,8 @@ if (NULL == getEnv("INPUT")) {
     $arrFiles[] = getEnv("INPUT");
 }
 
-Logger::verbose("Files to process: {}", array(
-    $arrFiles
-));
+Logger::verbose("Files to process: {}", $arrFiles);
+
 foreach ($arrFiles as $file) {
     $conversion = new ConvertFile("/data/" . $file, getEnv("TITLE"), getEnv("YEAR"), getEnv("SEASON"), getEnv("EPISODE"), getEnv("SUBTITLE"));
     $result = $conversion->convert();
