046088aa8d5aeb462eb67237ab69d5a066b2dff3
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-19 08:41:18 -0400

    Default audioSampleRate to NULL

--- php/Request.php
+++ php/Request.php
@@ -165,7 +165,7 @@ class Request
 
     private $audioChannelLayoutTracks = array();
 
-    public $audioSampleRate;
+    public $audioSampleRate = NULL;
 
     public $normalizeAudioTracks = NULL;
 
