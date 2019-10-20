4206dfd11ae1badf03f7206017c32302a85ebaef
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2019-10-20 08:27:43 -0400

    Squash issue.63 (#65)
    * Change the order of arguments to FFMpeg
    
    * Put hte loop in video
    
    * Loop inside generateAudioArgs
    
    * Loop within generateSubtitleArgs
    
    * Cleanup
    
    * Actually pass listRequests
    
    * Moved location of file
    
    * Imports
    
    * Generator iterface
    
    * Missing fields
    
    * Implmenet video args
    
    * Generic generateArgs method
    
    * Actually use generic for video
    
    * Fixed error in var name
    
    * Fix include
    
    * Moved to generators folder
    
    * Fix imports
    
    * Specify type
    
    * Audio generator
    
    * Use audio generator
    
    * Try adding php into include path
    
    * Use copy so that subdirectories are done correctly
    
    * Revert the include_path
    
    * Use correct var name
    
    * Print info
    
    * Start with blank args
    
    * Debug the streams
    
    * Show which file
    
    * Set args at the start not within the loop
    
    * Provide input track
    
    * Use DEBUG for commands
    
    * Added debug statements

---
+++ php/ffmpeg/generators/FFmpegVideoArgGenerator.php
@@ -0,0 +1,34 @@
+<?php
+include_once "ffmpeg/generators/FFmpegArgGenerator.php";
+include_once "InputFile.php";
+include_once "Request.php";
+include_once "Stream.php";
+
+class FFmpegVideoArgGenerator implements FFmpegArgGenerator
+{
+
+    public function getAdditionalArgs($outTrack, Request $request, $inputTrack, Stream $stream)
+    {
+	$args = " ";
+        if ("copy" == $request->videoFormat) {
+            $args .= " -c:v:" . $outTrack . " copy";
+        } else if ($request->isHDR()) {
+            $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10";
+        } else if ($request->isHwaccel()) {
+            $args .= " -c:v:" . $outTrack . " hevc_vaapi -qp 20 -level:v 41";
+            if ($request->deinterlace) {
+                $args .= " -vf deinterlace_vaapi=rate=field:auto=1";
+            }
+        } else {
+            $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 41";
+        }
+        $args .= " -metadata:s:v:" . $outTrack . " language=" . $stream->language;
+        return $args;
+    }
+
+    public function getStreams(InputFile $inputFile)
+    {
+        return $inputFile->getVideoStreams();
+    }
+}
+?>
