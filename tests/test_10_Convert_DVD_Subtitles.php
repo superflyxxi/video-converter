1191fd05434778dd6a31e1e3cfa478458c4112b6
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-26 19:45:06 -0400

    Test for correct streams

--- tests/test_10_Convert_DVD_Subtitles.php
+++ tests/test_10_Convert_DVD_Subtitles.php
@@ -3,7 +3,7 @@ include_once "common.php";
 
 getFile("dvd.mkv", "https://superflyxxi.dlinkddns.com/samples/DVD_Sample.mkv");
 
-$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Convert DVD Subtitle" -e VIDEO_TRACKS="" -e AUDIO_TRACKS="" -e SUBTITLE_FORMAT=srt -e YEAR=2019 ' . $image;
+$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Convert DVD Subtitle" -e VIDEO_TRACKS=" " -e AUDIO_TRACKS=" " -e SUBTITLE_FORMAT=srt -e YEAR=2019 ' . $image;
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 
@@ -12,8 +12,9 @@ test("ffmpeg code", 0, $return, $output);
 $probe = probe("/data/Test Convert Subtitle (2019).mkv");
 $probe = json_decode($probe, true);
 
-test("Stream 0", "subtitle", $probe["streams"][3]["codec_type"], $output);
-test("Stream 0 codec", "srt", $probe["streams"][3]["codec_name"], $output);
+test("Stream 0", "subtitle", $probe["streams"][0]["codec_type"], $output);
+test("Stream 0 codec", "srt", $probe["streams"][0]["codec_name"], $output);
+test("Stream 1 exists", FALSE, array_key_exists(1, $probe["streams"]), $output);
 test("Metadata Title", "Test Convert DVD Subtitle", $probe["format"]["tags"]["title"], $output);
 test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
 test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
--- tests/test_11_Convert_Bluray_Subtitles.php
+++ tests/test_11_Convert_Bluray_Subtitles.php
@@ -3,7 +3,7 @@ include_once "common.php";
 
 getFile("bluray.mkv", "https://superflyxxi.dlinkddns.com/samples/Bluray_Sample.mkv");
 
-$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=bluray.mkv -e TITLE="Test Convert Bluray Subtitle" -e VIDEO_TRACKS="" -e AUDIO_TRACKS="" -e SUBTITLE_FORMAT=ass -e YEAR=2019 ' . $image;
+$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=bluray.mkv -e TITLE="Test Convert Bluray Subtitle" -e VIDEO_TRACKS=" " -e AUDIO_TRACKS=" " -e SUBTITLE_FORMAT=ass -e YEAR=2019 ' . $image;
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 
@@ -12,8 +12,9 @@ test("ffmpeg code", 0, $return, $output);
 $probe = probe("/data/Test Convert Bluray Subtitle (2019).mkv");
 $probe = json_decode($probe, true);
 
-test("Stream 0", "subtitle", $probe["streams"][3]["codec_type"], $output);
-test("Stream 0 codec", "ass", $probe["streams"][3]["codec_name"], $output);
+test("Stream 0", "subtitle", $probe["streams"][0]["codec_type"], $output);
+test("Stream 0 codec", "ass", $probe["streams"][0]["codec_name"], $output);
+test("Stream 1 exists", FALSE, array_key_exists(1, $probe["streams"]), $output);
 test("Metadata Title", "Test Convert Bluray Subtitle", $probe["format"]["tags"]["title"], $output);
 test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
 test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
