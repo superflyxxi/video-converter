442c700c4161475e8c9e0ebf0d2144c25e912f79
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-26 09:36:54 -0400

    Initial file

---
+++ tests/test_11_ConvertMovie.php
@@ -0,0 +1,35 @@
+<?php
+include_once "common.php";
+
+getFile("movie.mkv", "https://superflyxxi.dlinkddns.com/samples/Movie_Sample.mkv");
+
+$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=movie.mkv -e TITLE="Test Convert Movie" -e YEAR=2019 ' . $image;
+printf("executing: %s\n", $command);
+exec($command, $output, $return);
+
+test("ffmpeg code", 0, $return, $output);
+
+$probe = probe("/data/Test Convert Movie (2019).mkv");
+$probe = json_decode($probe, true);
+
+printf("Probed JSON: \n%s\n", $probe);
+test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
+test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"], $output);
+test("Stream 1", "audio", $probe["streams"][1]["codec_type"], $output);
+test("Stream 1 codec", "aac", $probe["streams"][1]["codec_name"], $output);
+test("Stream 1 channels", 6, $probe["streams"][1]["channels"], $output);
+test("Stream 1 channel_layout", "5.1", $probe["streams"][1]["channel_layout"], $output);
+test("Stream 2", "audio", $probe["streams"][2]["codec_type"], $output);
+test("Stream 2 codec", "aac", $probe["streams"][2]["codec_name"], $output);
+test("Stream 2 channels", 6, $probe["streams"][2]["channels"], $output);
+test("Stream 2 channel_layout", "5.1", $probe["streams"][2]["channel_layout"], $output);
+test("Stream 3", "subtitle", $probe["streams"][3]["codec_type"], $output);
+test("Stream 3 codec", "ass", $probe["streams"][3]["codec_name"], $output);
+test("Metadata Title", "Test Convert Subtitle", $probe["format"]["tags"]["title"], $output);
+test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
+test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
+test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $output);
+test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]), $output);
+
+?>
+
