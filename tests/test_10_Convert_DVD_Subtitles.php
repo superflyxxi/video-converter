ed66267e575db7d7c37d6a30331a76102afcef31
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-26 19:22:54 -0400

    Convert DVD subtitles

--- tests/test_10_Convert_DVD_Subtitles.php
+++ tests/test_10_Convert_DVD_Subtitles.php
@@ -3,7 +3,7 @@ include_once "common.php";
 
 getFile("dvd.mkv", "https://superflyxxi.dlinkddns.com/samples/DVD_Sample.mkv");
 
-$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Convert Subtitle" -e VIDEO_TRACKS="" -e AUDIO_TRACKS="" -e SUBTITLE_FORMAT=srt -e YEAR=2019 ' . $image;
+$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Convert DVD Subtitle" -e VIDEO_TRACKS="" -e AUDIO_TRACKS="" -e SUBTITLE_FORMAT=srt -e YEAR=2019 ' . $image;
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 
@@ -12,15 +12,9 @@ test("ffmpeg code", 0, $return, $output);
 $probe = probe("/data/Test Convert Subtitle (2019).mkv");
 $probe = json_decode($probe, true);
 
-test("Stream 3", "subtitle", $probe["streams"][3]["codec_type"], $output);
-test("Stream 3 codec", "srt", $probe["streams"][3]["codec_name"], $output);
-test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
-test("Stream 0 codec", "mpeg2video", $probe["streams"][0]["codec_name"], $output);
-test("Stream 1", "audio", $probe["streams"][1]["codec_type"], $output);
-test("Stream 1 codec", "ac3", $probe["streams"][1]["codec_name"], $output);
-test("Stream 1 channels", 6, $probe["streams"][1]["channels"], $output);
-test("Stream 1 channel_layout", "5.1(side)", $probe["streams"][1]["channel_layout"], $output);
-test("Metadata Title", "Test Convert Subtitle", $probe["format"]["tags"]["title"], $output);
+test("Stream 0", "subtitle", $probe["streams"][3]["codec_type"], $output);
+test("Stream 0 codec", "srt", $probe["streams"][3]["codec_name"], $output);
+test("Metadata Title", "Test Convert DVD Subtitle", $probe["format"]["tags"]["title"], $output);
 test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
 test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
 test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $output);
