ea8ccf55d1fb802d0788a0c8a6da7364373380bf
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-27 09:25:02 -0400

    Change expected

--- tests/test_08_Auto_Deinterlace.php
+++ tests/test_08_Auto_Deinterlace.php
@@ -12,12 +12,10 @@ test("ffmpeg code", 0, $return, $output);
 $probe = probe("/data/Test Auto Deinterlace (2019).dvd.mkv.mkv");
 $probe = json_decode($probe, true);
 
-print_r($probe);
-
 test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
 test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"], $output);
 test("Stream 1 exists", FALSE, array_key_exists(1, $probe["streams"]), $output);
-test("Metadata Title", "Test Normalize Track 1", $probe["format"]["tags"]["title"], $output);
+test("Metadata Title", "Test Auto Deinterlace", $probe["format"]["tags"]["title"], $output);
 test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
 test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
 test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $output);
