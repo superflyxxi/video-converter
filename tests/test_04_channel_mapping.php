f87de8ea2a16eff28df44cfdb5d6f99a281bd0c3
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-26 20:20:05 -0400

    Use DVD

--- tests/test_04_channel_mapping.php
+++ tests/test_04_channel_mapping.php
@@ -1,18 +1,18 @@
 <?php
 include_once "common.php";
 
-getFile("test.mpg", "https://alcorn.com/wp-content/downloads/test-files/AC3AlcornTest_HD.mpg");
+getFile("dvd.mkv", "https://superflyxxi.dlinkddns.com/samples/DVD_Sample.mkv");
 
-$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e INPUT=test.mpg -e TITLE="Test Channel Mapping" -e YEAR=2019 -e AUDIO_CHANNEL_LAYOUT=stereo -e AUDIO_CHANNEL_LAYOUT_TRACKS=1 ' . $image;
+$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e INPUT=dvd.mkv -e TITLE="Test Channel Mapping" -e YEAR=2019 -e AUDIO_CHANNEL_LAYOUT=stereo -e AUDIO_CHANNEL_LAYOUT_TRACKS=1 -e VIDEO_FORMAT=copy -e SUBTITLE_FORMAT=copy ' . $image;
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 test("ffmpeg code", 0, $return, $output);
 
-$probe = probe("/data/Test Channel Mapping (2019).test.mpg.mkv");
+$probe = probe("/data/Test Channel Mapping (2019).dvd.mkv.mkv");
 $probe = json_decode($probe, true);
 
 test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
-test("Stream 0 codec", "hevc", $probe["streams"][0]["codec_name"], $output);
+test("Stream 0 codec", "mpeg2video", $probe["streams"][0]["codec_name"], $output);
 test("Stream 1", "audio", $probe["streams"][1]["codec_type"], $output);
 test("Stream 1 codec", "aac", $probe["streams"][1]["codec_name"], $output);
 test("Stream 1 channel_layout", "stereo", $probe["streams"][1]["channel_layout"], $output);
