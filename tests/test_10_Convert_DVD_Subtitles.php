429a8a4a98405a6c716a607a71dadccfd645ae33
SuperFlyXXI <superflyxxi@yahoo.com>
2019-10-27 16:31:06 -0400

    Only convert subtitles

--- tests/test_10_Convert_DVD_Subtitles.php
+++ tests/test_10_Convert_DVD_Subtitles.php
@@ -3,7 +3,7 @@ include_once "common.php";
 
 getFile("dvd.mkv", "https://superflyxxi.dlinkddns.com/samples/DVD_Sample.mkv");
 
-$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Convert DVD Subtitle" -e VIDEO_TRACKS=" " -e AUDIO_TRACKS=" " -e SUBTITLE_FORMAT=srt -e YEAR=2019 ' . $image;
+$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Convert DVD Subtitle" -e VIDEO_TRACKS=-1 -e AUDIO_TRACKS=-1 -e SUBTITLE_FORMAT=srt -e YEAR=2019 ' . $image;
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 
