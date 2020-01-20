b2411341ac2d434b07f09d448b48cd01d263467c
SuperFlyXXI <superflyxxi@yahoo.com>
2020-01-19 19:14:51 -0500

    Default SUBTITLE_FORMAT to copy

--- php/Request.php
+++ php/Request.php
@@ -18,7 +18,7 @@ class Request
 
         $req->playlist = getEnvWithDefault("PLAYLIST", NULL);
         $req->setSubtitleTracks(getEnvWithDefault("SUBTITLE_TRACKS", "*"));
-        $req->subtitleFormat = getEnvWithDefault("SUBTITLE_FORMAT", "ass");
+        $req->subtitleFormat = getEnvWithDefault("SUBTITLE_FORMAT", "copy");
         $req->subtitleConversionOutput = getEnvWithDefault("SUBTITLE_CONVERSION_OUTPUT", "MERGE");
 
         $req->setAudioTracks(getEnvWithDefault("AUDIO_TRACKS", "*"));
