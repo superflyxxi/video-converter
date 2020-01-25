21f1088af22a46431bbbc4261bedc610b740b824
SuperFlyXXI <superflyxxi@yahoo.com>
2020-01-25 17:59:54 -0500

    Conditional test

--- tests/test_12_Subtitle_Files.php
+++ tests/test_12_Subtitle_Files.php
@@ -7,24 +7,25 @@ $command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTF
 printf("executing: %s\n", $command);
 exec($command, $output, $return);
 
-/*if ( getEnv("BUILD_SUBTITLE_CONVERT") == "false") {
-	test("ffmpeg failed for a different reason", 0, $return, $output);
-} else {*/
-	test("ffmpeg code", 0, $return, $output);
+test("ffmpeg code", 0, $return, $output);
 
-	$probe = probe("/data/Test Subtitle Files (2019).mkv");
-	$probe = json_decode($probe, true);
+$probe = probe("/data/Test Subtitle Files (2019).mkv");
+$probe = json_decode($probe, true);
 
-	test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
+test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
+if ( getEnv("BUILD_SUBTITLE_CONVERT") == "false") {
+	test("Stream 1 exists", TRUE, array_key_exists(1, $probe["streams"]), array($probe, $output));
+	test("Stream 1 codec", "hdmv_pgs_subtitle", $probe["streams"][1]["codec_name"], $output);
+} else {
 	test("Stream 1 exists", FALSE, array_key_exists(1, $probe["streams"]), array($probe, $output));
-	test("Metadata Title", "Test Subtitle Files", $probe["format"]["tags"]["title"], $output);
-	test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
-	test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
-	test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $output);
-	test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]), $output);
+}
+test("Metadata Title", "Test Subtitle Files", $probe["format"]["tags"]["title"], $output);
+test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
+test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
+test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $output);
+test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]), $output);
 
-	$testfile = getEnv("TMP_DIR")."/Test Subtitle Files (2019).mkv.2-eng.srt";
-	test("File exists, ".$testfile, TRUE, file_exists($testfile));
-//}
+$testfile = getEnv("TMP_DIR")."/Test Subtitle Files (2019).mkv.2-eng.srt";
+test("File exists, ".$testfile, getEnv("BUILD_SUBTITLE_CONVERT") != "false", file_exists($testfile));
 ?>
 
