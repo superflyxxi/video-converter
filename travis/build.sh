111e46e0d833000b0ad332ee31735c2f7d2511f4
SuperFlyXXI <superflyxxi@yahoo.com>
2020-02-02 09:15:12 -0500

    Build different dockerfiles from build images

--- travis/build.sh
+++ travis/build.sh
@@ -9,5 +9,5 @@ find . -name *.php | xargs -L1 php -l
 
 CACHE_FROM_ARGS=${CACHE_FROM_ARGS:-"--cache-from ${CACHE_FROM_IMAGE}"}
 
-docker build ${CACHE_FROM_ARGS} --build-arg BUILD_SUBTITLE_CONVERT=${BUILD_SUBTITLE_CONVERT:-true} --build-arg FROM_IMAGE=${FROM_IMAGE} --tag ${THIS_FULL_IMAGE} -f Dockerfile.${FFMPEG_DOCKER_LABEL} .
+docker build ${CACHE_FROM_ARGS} --build-arg BUILD_SUBTITLE_CONVERT=${BUILD_SUBTITLE_CONVERT:-true} --build-arg FROM_IMAGE=${FROM_IMAGE} --tag ${THIS_FULL_IMAGE} -f Dockerfile.${THIS_DOCKERFILE} .
 
