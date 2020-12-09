85398dbbc9cd420d950f7e1d6c8af68c93193afb
SuperFlyXXI <superflyxxi@yahoo.com>
2020-12-09 11:16:03 -0500

    Fixed to actually work correctly

--- tests/common.php
+++ tests/common.php
@@ -49,12 +49,12 @@ function getFile($localFilename, $URL)
     return TRUE;
 }
 
-function test_ffmpeg($envVars, &$output, &$return, $time = "5m") {
-    $command = 'timeout -s9 ' . $timeout . ' docker run --rm -t --name test -v ' . getEnv("TMP_DIR") . ':/data ';
+function test_ffmpeg($envVars, &$output, &$return, $timeout = "5m") {
+    $command = 'timeout -s9 ' . $timeout . ' docker run --rm -t --name test -v "' . getEnv("TMP_DIR") . ':/data" ';
     foreach ($envVars as $key => $value) {
         $command .= " -e " . $key . '="' . $value . '" ';
     }
-    $command .= $image;
+    $command .= getEnv("THIS_FULL_IMAGE");
     printf("executing: %s\n", $command);
     exec($command, $output, $return);
     exec("docker stop test");
--- tests/test_08_Auto_Deinterlace.php
+++ tests/test_08_Auto_Deinterlace.php
@@ -4,16 +4,10 @@ include_once "common.php";
 getFile("dvd.mkv", "https://".$sampleDomain."/samples/DVD_Sample.mkv");
 
 test_ffmpeg(array("INPUT"=>"dvd.mkv", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Auto Deinterlace", "YEAR"=>2019), $output, $return);
-//$command = 'docker run --rm -t --name test08 -v ' . getEnv("TMP_DIR") . ':/data -e INPUT=dvd.mkv -e AUDIO_TRACKS=-1 -e SUBTITLE_TRACKS=-1 -e TITLE="Test Auto Deinterlace" -e YEAR=2019 ' . $image;
-//printf("executing: %s\n", $command);
-//exec("timeout -s9 5m ". $command, $output, $return);
-//exec("docker stop test08");
 
 //test("ffmpeg code", 0, $return, $output);
 
 $probe = probe("/data/Test Auto Deinterlace (2019).dvd.mkv.mkv");
-print_r("Result of probe: ");
-print_r($probe);
 $probe = json_decode($probe, true);
 
 test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
@@ -26,4 +20,3 @@ test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["ta
 test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]), $output);
 
 ?>
-
