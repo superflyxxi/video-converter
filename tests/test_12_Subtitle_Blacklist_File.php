178c5a2ea90f47af18af4e357f852e4b7d94a69d
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2020-12-10 14:06:29 -0500

    Use common docker instead of building own (#84)
    * Support new file
    
    * Initial attempt
    
    * Initial build success
    
    * Passthrough the output
    
    * No longer have more than one image
    
    * Avoid the warnings during builds
    
    * Timeout for 5m
    
    * Introduce a common test_ffmpeg method and use it once
    
    * Fixed to actually work correctly
    
    * Convert test 00
    
    * Don't automatically remove
    
    * Provide some debugging
    
    * Attempt to fix something?
    
    * Print the probe results
    
    * test_ffmpeg test 2
    
    * Try skipping this test
    
    * Fixed json decoding
    
    * Moved back to original
    
    * Use common
    
    * Use test_ffmpeg
    
    * use testoutput
    
    * Use stop and always rm container
    
    * Don't print JSON
    
    * User/Group
    
    * Removed comments
    
    * Add some cleans
    
    * Auto remove and purge
    
    * Use noninteractive to avoid warning
    
    * Use array_merge
    
    * Removed support for latest
    
    * Removed subtitle build stuff
    
    * Make the script executable
    
    * Renamed to rip-video
    
    * Use DOCKERFILE
    
    * Remove scripts
    
    * Use renamed script
    
    * Use appropriate exports

--- tests/test_12_Subtitle_Blacklist_File.php
+++ tests/test_12_Subtitle_Blacklist_File.php
@@ -3,34 +3,34 @@ include_once "common.php";
 
 getFile("dvd.mkv", "https://".$sampleDomain."/samples/DVD_Sample.mkv");
 
-$command = 'docker run --rm -t -v ' . getEnv("TMP_DIR") . ':/data -e APPLY_POSTFIX=false -e INPUT=dvd.mkv -e TITLE="Test Subtitle Files" -e VIDEO_FORMAT=copy -e AUDIO_TRACKS=-1 -e SUBTITLE_FORMAT=srt -e SUBTITLE_CONVERSION_OUTPUT=FILE -e SUBTITLE_CONVERSION_BLACKLIST="!\’" -e YEAR=2019 ' . $image;
-printf("executing: %s\n", $command);
-exec($command, $output, $return);
+test_ffmpeg(array("APPLY_POSTFIX"=>"false", "INPUT"=>"dvd.mkv", "TITLE"=>"Test Subtitle Files", "VIDEO_FORMAT"=>"copy", "AUDIO_TRACKS"=>-1, "SUBTITLE_FORMAT"=>"srt", "SUBTITLE_CONVERSION_OUTPUT"=>"FILE", "SUBTITLE_CONVERSION_BLACKLIST"=>"!\�~@~", "YEAR"=>2019), $output, $return);
 
 test("ffmpeg code", 0, $return, $output);
 
 $probe = probe("/data/Test Subtitle Files (2019).mkv");
 $probe = json_decode($probe, true);
 
-test("Stream 0", "video", $probe["streams"][0]["codec_type"], $output);
+$testOutput = array($output, $probe);
+
+test("Stream 0", "video", $probe["streams"][0]["codec_type"], $testOutput);
 if ( getEnv("BUILD_SUBTITLE_CONVERT") == "false") {
-	test("Stream 1 exists", TRUE, array_key_exists(1, $probe["streams"]), array($probe, $output));
-	test("Stream 1 codec", "dvd_subtitle", $probe["streams"][1]["codec_name"], $output);
+	test("Stream 1 exists", TRUE, array_key_exists(1, $probe["streams"]), array($probe, $testOutput));
+	test("Stream 1 codec", "dvd_subtitle", $probe["streams"][1]["codec_name"], $testOutput);
 } else {
-	test("Stream 1 exists", FALSE, array_key_exists(1, $probe["streams"]), array($probe, $output));
+	test("Stream 1 exists", FALSE, array_key_exists(1, $probe["streams"]), array($probe, $testOutput));
 }
-test("Metadata Title", "Test Subtitle Files", $probe["format"]["tags"]["title"], $output);
-test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $output);
-test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $output);
-test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $output);
-test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]), $output);
+test("Metadata Title", "Test Subtitle Files", $probe["format"]["tags"]["title"], $testOutput);
+test("Metadata YEAR", "2019", $probe["format"]["tags"]["YEAR"], $testOutput);
+test("Metadata SEASON", FALSE, array_key_exists("SEASON", $probe["format"]["tags"]), $testOutput);
+test("Metadata EPISODE", FALSE, array_key_exists("EPISODE", $probe["format"]["tags"]), $testOutput);
+test("Metadata SUBTITLE", FALSE, array_key_exists("SUBTITLE", $probe["format"]["tags"]), $testOutput);
 
 $testfile = getEnv("TMP_DIR")."/Test Subtitle Files (2019).mkv.2-eng.srt";
 test("File exists, ".$testfile, getEnv("BUILD_SUBTITLE_CONVERT") != "false", file_exists($testfile));
 if (getEnv("BUILD_SUBTITLE_CONVERT") != "false") {
 	$contents = file_get_contents($testfile);
-	test("SRT Contains ’", FALSE, strpos($contents, "’"), implode("\n", $output) . "\n" . $contents);
-	test("SRT Contains !", FALSE, strpos($contents, "!"), implode("\n", $output) . "\n" . $contents);
+	test("SRT Contains ’", FALSE, strpos($contents, "’"), array_merge($testOutput, array($contents)));
+	test("SRT Contains !", FALSE, strpos($contents, "!"), array_merge($testOutput, array($contents)));
 }
 ?>
 
