66514dc8dce5f3545d05e8a1c1344588376615ad
SuperFlyXXI <superflyxxi@yahoo.com>
2020-12-30 14:19:55 -0500

    Fixed error

--- php/ConvertSubtitle.php
+++ php/ConvertSubtitle.php
@@ -22,8 +22,8 @@ class ConvertSubtitle
 		try {
                     if ("hdmv_pgs_subtitle" == $codecName) {
                         // convert to dvd
-                       if ($oRequest->oInputFile->getPrefix() != NULL) {
-                           $dvdFile = $dir . "/" . realpath($filename) . "/dir-" . $index;
+                        if ($oRequest->oInputFile->getPrefix() != NULL) {
+                            $dvdFile = $dir . "/" . realpath($filename) . "/dir-" . $index;
                         } else {
                             $dvdFile = $dir . "/" . $filename . '-' . $index;
                         }
@@ -41,79 +41,76 @@ class ConvertSubtitle
 
                         if (! file_exists($dvdFile . '.sub')) {
                             Logger::info("Converting pgs to dvd subtitle.");
-                        $command = 'java -jar /home/ripvideo/BDSup2Sub.jar -o "' . $dvdFile . '.sub" "' . $pgsFile->getFileName() . '"';
-                        Logger::debug("Command: {}", $command);
-                        exec($command, $out, $return);
-                        if ($return != 0) {
-                            throw new ExecutionException("java", $return, $command);
+                            $command = 'java -jar /home/ripvideo/BDSup2Sub.jar -o "' . $dvdFile . '.sub" "' . $pgsFile->getFileName() . '"';
+                            Logger::debug("Command: {}", $command);
+                            exec($command, $out, $return);
+                            if ($return != 0) {
+                                throw new ExecutionException("java", $return, $command);
+                            }
                         }
-                    }
-                } else if ("vobsub" == $codecName || "dvd_subtitle" == $codecName) {
-                    // extract vobsub
-                    if ($oRequest->oInputFile->getPrefix() != NULL) {
-                        $dvdFile = $dir . "/" . realpath($filename) . "/dir-" . $index;
-                    } else {
-                        $dvdFile = $dir . "/" . $filename . '-' . $index;
-                    }
-                    if (! file_exists($dvdFile . ".sub")) {
-                        Logger::info("Generating DVD sub file for index {} of file {}.", $index, $filename);
-                        $arrOutput = array(
-                            $index => $dvdFile . ".sub"
-                        );
-                        MKVExtractHelper::extractTracks($oRequest->oInputFile, $arrOutput);
-                    }
-                } else if ("subrip" == $codecName) {
-                    Logger::info("Adding subrip to request for track {} of file {}", $index, $oRequest->oInputFile->getFileName());
-                    $oNewRequest = new Request($oRequest->oInputFile->getFileName());
-                    $oNewRequest->setSubtitleTracks($index);
-                    $oNewRequest->subtitleFormat = $oRequest->subtitleFormat;
-                    $oNewRequest->setAudioTracks(NULL);
-                    $oNewRequest->setVideoTracks(NULL);
-                    $oNewRequest->prepareStreams();
-                    $arrAdditionalRequests[] = $oNewRequest;
-                    $oRequest->oInputFile->removeSubtitleStream($index);
-                }
-
-                // convert to srt
-                if (NULL != $dvdFile) {
-                    if (! file_exists($dvdFile . ".srt")) {
-                        Logger::info("Convert DVD sub to SRT.");
-                        $command = 'vobsub2srt ';
-                        if (isset($subtitle->language)) {
-                            $command .= ' --tesseract-lang ' . $subtitle->language . ' ';
+                    } else if ("vobsub" == $codecName || "dvd_subtitle" == $codecName) {
+                        // extract vobsub
+                        if ($oRequest->oInputFile->getPrefix() != NULL) {
+                            $dvdFile = $dir . "/" . realpath($filename) . "/dir-" . $index;
+                        } else {
+                            $dvdFile = $dir . "/" . $filename . '-' . $index;
                         }
-                        if (NULL != $oRequest->subtitleConversionBlacklist) {
-                            $command .= ' --blacklist "' . $oRequest->subtitleConversionBlacklist . '"';
+                        if (! file_exists($dvdFile . ".sub")) {
+                            Logger::info("Generating DVD sub file for index {} of file {}.", $index, $filename);
+                            $arrOutput = array($index => $dvdFile . ".sub");
+                            MKVExtractHelper::extractTracks($oRequest->oInputFile, $arrOutput);
                         }
-                        $command .= ' "' . $dvdFile . '" ';
-                        Logger::debug("Command: {}", $command);
-                        exec($command, $out, $return);
-                        if ($return != 0) {
-			    Logger::warn("Failed to convert {}. returned {}: {}", $dvdFile, $return, $out);
-			    continue;
-                        }
-                    }
-                    if ($oRequest->subtitleConversionOutput == "MERGE") {
-                        Logger::info("Merging srt, {}.srt, into mkv.", $dvdFile);
-                        $oNewRequest = new Request($dvdFile . ".srt");
-                        $oNewRequest->setSubtitleTracks("0");
+                    } else if ("subrip" == $codecName) {
+                        Logger::info("Adding subrip to request for track {} of file {}", $index, $oRequest->oInputFile->getFileName());
+                        $oNewRequest = new Request($oRequest->oInputFile->getFileName());
+                        $oNewRequest->setSubtitleTracks($index);
+                        $oNewRequest->subtitleFormat = $oRequest->subtitleFormat;
                         $oNewRequest->setAudioTracks(NULL);
                         $oNewRequest->setVideoTracks(NULL);
-                        $oNewRequest->subtitleFormat = $oRequest->subtitleFormat;
                         $oNewRequest->prepareStreams();
-                        $oNewRequest->oInputFile->getSubtitleStreams()[0]->language = $subtitle->language;
-                        Logger::debug("Using {} language for final stream.", $subtitle->language);
                         $arrAdditionalRequests[] = $oNewRequest;
-                    } else {
-                        $newFile = $oOutput->getFileName() . "." . $index . "-" . $subtitle->language . ".srt";
-                        Logger::info("Keeping file, {}.srt, outside as {}", $dvdFile, $newFile);
-                        rename($dvdFile . ".srt", $newFile);
+                        $oRequest->oInputFile->removeSubtitleStream($index);
                     }
-                    $oRequest->oInputFile->removeSubtitleStream($index);
+
+                    // convert to srt
+                    if (NULL != $dvdFile) {
+                        if (! file_exists($dvdFile . ".srt")) {
+                            Logger::info("Convert DVD sub to SRT.");
+                            $command = 'vobsub2srt ';
+                            if (isset($subtitle->language)) {
+                                $command .= ' --tesseract-lang ' . $subtitle->language . ' ';
+                            }
+                            if (NULL != $oRequest->subtitleConversionBlacklist) {
+                                $command .= ' --blacklist "' . $oRequest->subtitleConversionBlacklist . '"';
+                            }
+                            $command .= ' "' . $dvdFile . '" ';
+                            Logger::debug("Command: {}", $command);
+                            exec($command, $out, $return);
+                            if ($return != 0) {
+                                throw new ExecutionException("vobsub2srt", $return, $command);
+                            }
+                        }
+                        if ($oRequest->subtitleConversionOutput == "MERGE") {
+                            Logger::info("Merging srt, {}.srt, into mkv.", $dvdFile);
+                            $oNewRequest = new Request($dvdFile . ".srt");
+                            $oNewRequest->setSubtitleTracks("0");
+                            $oNewRequest->setAudioTracks(NULL);
+                            $oNewRequest->setVideoTracks(NULL);
+                            $oNewRequest->subtitleFormat = $oRequest->subtitleFormat;
+                            $oNewRequest->prepareStreams();
+                            $oNewRequest->oInputFile->getSubtitleStreams()[0]->language = $subtitle->language;
+                            Logger::debug("Using {} language for final stream.", $subtitle->language);
+                            $arrAdditionalRequests[] = $oNewRequest;
+                        } else {
+                            $newFile = $oOutput->getFileName() . "." . $index . "-" . $subtitle->language . ".srt";
+                            Logger::info("Keeping file, {}.srt, outside as {}", $dvdFile, $newFile);
+                            rename($dvdFile . ".srt", $newFile);
+                        }
+                        $oRequest->oInputFile->removeSubtitleStream($index);
+                    }
+                } catch (ExectionException $ex) {
+	            Logger::warn("Skipping track due to error {}", $ex->getMessage());
                 }
-            } catch (ExectionException $ex) {
-		Logger::error("Skipping track due to error {}", $ex->getMessage());
-	    }
             }
             // if for some reason some couldn't be converted, copy the ones in the main input file
             $oRequest->subtitleFormat = "copy";
--- php/exceptions/ExecutionException.php
+++ php/exceptions/ExecutionException.php
@@ -7,7 +7,7 @@ class ExecutionException extends Exception {
     private $args = NULL;
 
     public function __construct(string $program, int $err, $args = NULL) {
-        parent::__construct("Program (" . $programe . " failed to execute.", $err);
+        parent::__construct("Program (" . $program . " failed to execute.", $err);
         $this->args = $args;
 	Logger::error($this->getMessage());
     }
