0e57355940c0684140b294cbd95a0bbfef401fa8
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-18 11:20:19 -0500

    Escape

--- test.sh
+++ test.sh
@@ -9,7 +9,10 @@ TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN:?Missing TEST_SAMPLE_DOMAIN}
 
 cd tests/
 export TMP_DIR=$(mktemp -d)
-export LOG_LEVEL=VERBOSE
+export LOG_LEVEL="VERBOSE"
+export GREP_IGNORE="*"
+if [[ ! "${BUILD_SUBTITLE_SUPPORT}" == "true" ]]; then
+	GREP_IGNORE="\(test_10\|test_11\|test_12\)"
+fi
 
-find . -name 'test*.php' | sort | xargs -L1 php
-
+find . -name 'test*.php' | grep -v "${GREP_IGNORE}" | sort | xargs -L1 php
