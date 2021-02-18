464348e636169977b8272509b54956f91a49b4a3
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-18 15:52:26 -0500

    Use filters before conversion

--- php/ffmpeg/generators/FFmpegVideoArgGenerator.php
+++ php/ffmpeg/generators/FFmpegVideoArgGenerator.php
@@ -15,17 +15,17 @@ class FFmpegVideoArgGenerator implements FFmpegArgGenerator
         } else if ($request->isHDR()) {
             $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10";
         } else if ($request->isHwaccel()) {
-            $args .= " -c:v:" . $outTrack . " hevc_vaapi -qp 20 -level:v 4";
             if ($request->deinterlace) {
                 //$args .= " -vf 'deinterlace_vaapi=rate=field:auto=1'"; // each field is a frame (double framerate) https://www.mltframework.org/plugins/FilterAvfilter-deinterlace_vaapi/
                 $args .= " -vf 'hwdownload,fieldmatch,yadif=deint=1,decimate,hwupload'"; // https://ffmpeg.org/ffmpeg-filters.html#fieldmatch
             }
+            $args .= " -c:v:" . $outTrack . " hevc_vaapi -qp 20 -level:v 4";
         } else {
-            $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 4";
             if ($request->deinterlace) {
                 //$args .= " -vf 'yadif=mode=1'"; // each field is a frame (double framerate) https://ffmpeg.org/ffmpeg-filters.html#yadif-1
                 $args .= " -vf 'fieldmatch,yadif=deint=1,decimate'"; // https://ffmpeg.org/ffmpeg-filters.html#fieldmatch
             }
+            $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 4";
         }
         $args .= " -metadata:s:v:" . $outTrack . " language=" . $stream->language;
         return $args;
