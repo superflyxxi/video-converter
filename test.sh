4e6e1a818c679fab252625e7bf15a92b718042c6
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-18 11:05:04 -0500

    Validate env variables

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@
 
 set -ex
 
+THIS_FULL_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE}
+TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN:?Missing TEST_SAMPLE_DOMAIN}
+
 cd tests/
 export TMP_DIR=$(mktemp -d)
 export LOG_LEVEL=VERBOSE
