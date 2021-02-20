53d87996093cb85beea42a08a81255ecb650dfae
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-19 19:54:37 -0500

    Probe de-interlacing

--- tests/test_08_Auto_Probe_Deinterlace_Mode_00.php
+++ tests/test_08_Auto_Probe_Deinterlace_Mode_00.php
@@ -3,11 +3,11 @@ include_once "common.php";
 
 getFile("dvd.mkv", "https://".$sampleDomain."/samples/DVD_Sample.mkv");
 
-test_ffmpeg(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Auto Deinterlace", "YEAR"=>2019), $output, $return);
+test_ffmpeg(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Probe Auto Deinterlace", "YEAR"=>2019), $output, $return);
 
 // not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
 
-$probe = probe("/data/Test Auto Deinterlace (2019).dvd.mkv.mkv");
+$probe = probe("/data/Test Probe Auto Deinterlace (2019).dvd.mkv.mkv");
 $probe = json_decode($probe, true);
 
 $testOutput = array($output, $probe);
