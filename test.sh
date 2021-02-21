f72ed70d5e3e2ba400bc53f85da651e5c2d56e55
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 09:28:50 -0500

    Untested docker builds

--- test.sh
+++ test.sh
@@ -5,14 +5,7 @@
 set -e
 
 THIS_FULL_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE}
-TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN:?Missing TEST_SAMPLE_DOMAIN}
 
 cd tests/
-export TMP_DIR=$(mktemp -d)
-export LOG_LEVEL="VERBOSE"
-export GREP_IGNORE="*"
-if [[ ! "${BUILD_SUBTITLE_SUPPORT}" == "true" ]]; then
-	GREP_IGNORE="\(test_10\|test_11\|test_12\)"
-fi
-
-find . -name 'test*.php' | grep -v "${GREP_IGNORE}" | sort | xargs -L1 php
+docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE} .
+docker run --rm -e LOG_LEVEL=VERBOSE -e BUILD_SUBTITLE_SUPPORT=${BUILD_SUBTITLE_SUPPORT} -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test
