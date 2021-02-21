8d77cb2b9233dc49bb9e6882bdd674f47e6d8de5
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 12:56:49 -0500

    Testing test

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Assuming FFMPEG_DOCKER set
-
-set -e
-
-THIS_FULL_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE}
-
-cd tests/
-docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE} .
-docker run --rm -e LOG_LEVEL=VERBOSE -e BUILD_SUBTITLE_SUPPORT=${BUILD_SUBTITLE_SUPPORT} -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test
+#docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE} tests/
+if [[ "${BUILD_SUBTITLE_SUPPORT}" != "true" ]]; then
+	TESTSUITES="basic"
+fi
+set -ex
+docker run --rm --user $(id -u):$(id -g) -v "$(pwd)/tests/:/tests/" -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test subtitle/SubtitleTests.php #--testsuite ${TESTSUITES}
+#docker run --rm --user $(id -u):$(id -g) -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test #--testsuite ${TESTSUITES}
