649f2807f6513199de095788e6859cf6176d75e2
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 15:51:42 -0500

    Support deinterlace test cases

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE} tests/
-if [[ "${BUILD_SUBTITLE_SUPPORT}" == "true" ]]; then
-	TESTSUITES="basic,subtitles"
-else
-	TESTSUITES="basic"
+TESTSUITES="basic,deinterlace"
+if [[ "${BUILD_SUBTITLE_SUPPORT}" = "true" ]]; then
+	TESTSUITES="${TESTSUITES},subtitles"
 fi
 set -ex
 docker run --name test --user $(id -u):$(id -g) -v "$(pwd)/tests/:/tests/" -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
