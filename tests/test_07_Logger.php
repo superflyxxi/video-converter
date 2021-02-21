eb5665fb41d404af402d1dd29c127aaf1c05bdaa
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 09:56:17 -0500

    Made it a php unit test case

--- tests/test_07_Logger.php
+++ tests/test_07_Logger.php
@@ -2,14 +2,18 @@
 include_once "common.php";
 include_once "Logger.php";
 
-Logger::debug("Testing");
-Logger::debug("Testing nothing", "blah");
-Logger::debug("Testing something {}", "blah");
-Logger::debug("Testing multiple things {}, {}, {}", "one", "two", "three");
-Logger::debug("Testing an array {}", array(
-    "something",
-    "somethingelse"
-));
-Logger::debug("Testing an integer={} and float={}", 1, 2.35);
+use PHPUnit\Framework\TestCase;
 
+final class LoggingTest extends TestCase
+{
+
+    public function testNoErrors() {
+        Logger::debug("Testing");
+        Logger::debug("Testing nothing", "blah");
+        Logger::debug("Testing something {}", "blah");
+        Logger::debug("Testing multiple things {}, {}, {}", "one", "two", "three");
+        Logger::debug("Testing an array {}", array("something","somethingelse"));
+        Logger::debug("Testing an integer={} and float={}", 1, 2.35);
+    }
+}
 ?>
