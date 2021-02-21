b39610e49a19bb540365b59d7c42b2bdc442c5f1
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 13:38:30 -0500

    Include logging into basic

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -3,9 +3,19 @@
  * Tests when no input is given. It should product results for the file as expected.
  */
 include_once "common.php";
+include_once "Logger.php";
 
 final class BasicTest extends Test
 {
+    public function testLoggingProducesNoErrors() {
+        Logger::debug("Testing");
+        Logger::debug("Testing nothing", "blah");
+        Logger::debug("Testing something {}", "blah");
+        Logger::debug("Testing multiple things {}, {}, {}", "one", "two", "three");
+        Logger::debug("Testing an array {}", array("something","somethingelse"));
+        Logger::debug("Testing an integer={} and float={}", 1, 2.35);
+        $this->assertTrue(true, "Test ran without error");
+    }
 
     public function testNoInputSpecified() {
         $this->getFile("dvd");
