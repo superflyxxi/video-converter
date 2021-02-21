e8ae6f9e2b71104d1759a04e2e29c66e8d798f23
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 13:51:05 -0500

    Got working testsuties

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE} tests/
-if [[ "${BUILD_SUBTITLE_SUPPORT}" != "true" ]]; then
+if [[ "${BUILD_SUBTITLE_SUPPORT}" == "true" ]]; then
+	TESTSUITES="basic,subtitles"
+else
 	TESTSUITES="basic"
 fi
 set -ex
-docker run --rm --user $(id -u):$(id -g) -v "$(pwd)/tests/:/tests/" -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test subtitle/SubtitleTests.php #--testsuite ${TESTSUITES}
+docker run --name test --user $(id -u):$(id -g) -v "$(pwd)/tests/:/tests/" -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
 #docker run --rm --user $(id -u):$(id -g) -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test #--testsuite ${TESTSUITES}
--- tests/common.php
+++ tests/common.php
@@ -2,7 +2,6 @@
 $user = getEnv("UID");
 $image = getEnv("THIS_FULL_IMAGE");
 set_include_path(get_include_path() . PATH_SEPARATOR . "/home/ripvideo");
-printf("TEST: %s\n", debug_backtrace()[0]['file']);
 
 function test($message, $expected, $actual, $extraLogs = "")
 {
