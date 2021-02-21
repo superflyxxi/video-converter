f1e1e5803255bfcbcd819cf6bdfe0d58bfad0bb2
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-21 16:32:29 -0500

    Removed commented out code

--- tests/common.php
+++ tests/common.php
@@ -1,25 +1,5 @@
 <?php
-$user = getEnv("UID");
-$image = getEnv("THIS_FULL_IMAGE");
 set_include_path(get_include_path() . PATH_SEPARATOR . "/home/ripvideo");
-
-function test($message, $expected, $actual, $extraLogs = "")
-{
-    if ($expected !== $actual) {
-        printf("FAIL: %s. Expected='", $message);
-        print_r($expected);
-        printf("', but got '");
-        print_r($actual);
-        printf("'\n");
-        print_r($extraLogs);
-        printf("\n\n");
-        flush();
-        exit(1);
-    }
-    printf("PASS: %s. Got expected='", $message);
-    print_r($expected);
-    printf("'\n");
-}
 
 use PHPUnit\Framework\TestCase;
 class Test extends TestCase {
@@ -34,15 +14,7 @@ class Test extends TestCase {
 
     public function probe($file)
     {
-        global $user;
-        global $image;
-        //$command = 'docker run --rm -t';
-        //if ($user) {
-        //    $command .= ' --user=' . $user;
-        //}
-        //$command .= ' -v ' . getEnv("TMP_DIR") . ':/data --entrypoint ffprobe ' . $image . ' -v quiet -print_format json -show_format -show_streams "' . $file . '"';
         $command = 'ffprobe -v quiet -print_format json -show_format -show_streams "' . $this->tmpDir . DIRECTORY_SEPARATOR . $file . '"';
-        //printf("Probing '%s'\nCommand: %s\n", $file, $command);
         exec($command, $out, $ret);
         if ($ret == 0) {
             $out = implode($out);
@@ -72,18 +44,12 @@ class Test extends TestCase {
     }
 
     public function ripvideo($envVars, &$output, &$return, $timeout = "8m") {
-        //$command = 'timeout -s9 ' . $timeout . ' docker run -t --rm --user $(id -u):$(id -g) --name test -v "' . getEnv("TMP_DIR") . ':/data" ';
         $command = "";
         foreach ($envVars as $key => $value) {
             $command .= $key . '="' . $value . '" ';
         }
         $command .= 'timeout -s9 ' . $timeout . ' /home/ripvideo/rip-video ';
-        //$command .= getEnv("THIS_FULL_IMAGE");
-        //printf("%s: executing: %s\n", date(DateTimeInterface::ISO8601), $command);
-        //passthru($command, $return);
         exec($command, $output, $return);
-        //exec("docker stop test");
-        //printf("%s: Done executing\n", date(DateTimeInterface::ISO8601));
-}
+    }
 }
 ?>
