3d46c3a89ffb5acac9d8c8deeed2793ab53c0724
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2021-02-22 10:10:31 -0500

    Test using docker (#95)
    * Untested docker builds
    
    * Allow for running tests
    
    * Use phpunit to test
    
    * Include required library
    
    * Made it a php unit test case
    
    * Match naming
    
    * Moved to basic
    
    * Definte testsuites
    
    * Group testsuites
    
    * Use 8 since ubuntu1804 only has 7.2
    
    * Renamed
    
    * Default to basic,subtitle
    
    * Enable first function
    
    * Convert passing the request
    
    * Include curl
    
    * Include home directory in path
    
    * Testing test
    
    * Make data directory
    
    * Support testing ffmpeg
    
    * Moved out of basic
    
    * Moved out of subtitle
    
    * Renamed to basic tests
    
    * Working
    
    * Working
    
    * Must end with Test
    
    * REnamed to end with test
    
    * Include logging into basic
    
    * Moved to folders
    
    * Made testsuite
    
    * Fixed test
    
    * Renamed to end with Test
    
    * Got working testsuties
    
    * Migrated test 02
    
    * Migrated test 03
    
    * Build test image
    
    * Test not applying postfix
    
    * Support deinterlace test cases
    
    * PHPUnit test 20
    
    * Renamed to plural
    
    * Moved 21 to phpunit
    
    * test 22 now phpunit
    
    * Migrated 23 to phpunit
    
    * Support audio testsuite
    
    * Converted test 4 to phpunit
    
    * test 5 phpunit
    
    * Fixed formats
    
    * test 10 now phpunit
    
    * Convert subtitles
    
    * Require mkv
    
    * Make it require
    
    * Removed commented out code
    
    * Silly circleCi
    
    * Specify the build args for test image
    
    * Don't volume mount
    
    * Default run everything
    
    * this registry
    
    * Fixed wrong levels being used
    
    * Removed unnecssary file
    
    * Don't overload TMP_DIR
    
    * Forgot this
    
    * Call it dataDir
    
    * Provide more details
    
    * Don't require logger
    
    * Since year was removed...
    
    * Ignore testResults
    
    * Save results
    
    * Print the results
    
    * Only need cli
    
    * Don't cache results
    
    * Use correct folder name
    
    * Clean up
    
    * Moved some things around
    
    * Use phpunit.xml for configs
    
    * Give the build a name
    
    * Remove the commented out code

---
+++ tests/deinterlace/DeinterlaceTest.php
@@ -0,0 +1,83 @@
+<?php
+require_once "common.php";
+
+final class DeinterlacesTests extends Test
+{
+
+    public function testProbeAutoDeinterlaceAndMode00() {
+        $this->getFile("dvd");
+
+        $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Probe Auto Deinterlace", "YEAR"=>2019), $output, $return);
+
+// not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
+        $this->assertEquals(0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("Test Probe Auto Deinterlace (2019).dvd.mkv.mkv");
+
+        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
+        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+// doesn't exist... test("Stream 0 field_order", "progressive", $probe["streams"][0]["field_order"], $testOutput);
+        $this->assertEquals("24000/1001", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
+        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Probe Auto Deinterlace", $probe["format"]["tags"]["title"], "Metadata title");
+        $this->assertEquals("2019", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
+        $this->assertFalse(array_key_exists("SEASON", $probe["format"]["tags"]), "Metadata SEASON");
+        $this->assertFalse(array_key_exists("EPISODE", $probe["format"]["tags"]), "Metadata EPISODE");
+        $this->assertFalse(array_key_exists("SUBTITLE", $probe["format"]["tags"]), "Metadata SUBTITLE");
+    }
+
+    public function testIdetAutoDeinterlaceMode00() {
+        $this->getFile("dvd");
+
+        $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "DEINTERLACE_CHECK"=>"idet", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Idet Auto Deinterlace", "YEAR"=>2021), $output, $return);
+
+// not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
+        $this->assertEquals(0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("Test Idet Auto Deinterlace (2021).dvd.mkv.mkv");
+
+        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
+        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+        $this->assertEquals("24000/1001", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
+        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Idet Auto Deinterlace", $probe["format"]["tags"]["title"], "Metadata title");
+        $this->assertEquals("2021", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
+    }
+
+    public function testDeinterlaceMode01() {
+        $this->getFile("dvd");
+
+        $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE"=>"true", "DEINTERLACE_MODE"=>"01", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Deinterlace Mode 01", "YEAR"=>2021), $output, $return);
+
+// not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
+        $this->assertEquals(0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("Test Deinterlace Mode 01 (2021).dvd.mkv.mkv");
+
+        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
+        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+        $this->assertEquals("19001/317", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
+        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Deinterlace Mode 01", $probe["format"]["tags"]["title"], "Metadata title");
+        $this->assertEquals("2021", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
+    }
+
+    public function testDeinterlaceMode02() {
+        $this->getFile("dvd");
+
+        $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE"=>"true", "DEINTERLACE_MODE"=>"02", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Deinterlace Mode 02", "YEAR"=>2021), $output, $return);
+
+// not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
+        $this->assertEquals(0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("Test Deinterlace Mode 02 (2021).dvd.mkv.mkv");
+
+        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
+        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+        $this->assertEquals("30000/1001", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
+        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Deinterlace Mode 02", $probe["format"]["tags"]["title"], "Metadata title");
+        $this->assertEquals("2021", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
+    }
+}
+?>
---
+++ tests/subtitles/SubtitleTest.php
@@ -0,0 +1,60 @@
+<?php
+require_once "common.php";
+
+final class SubtitleTests extends Test
+{
+
+    public function testDvdSubtitleConversion() {
+        $this->getFile("dvd");
+
+        $this->ripvideo(array("APPLY_POSTFIX"=>"false", "INPUT"=>"dvd.mkv", "TITLE"=>"Test Convert DVD Subtitle", "VIDEO_TRACKS"=>-1, "AUDIO_TRACKS"=>-1, "SUBTITLE_FORMAT"=>"srt", "YEAR"=>2019), $output, $return);
+
+        $this->assertEquals("ffmpeg code", 0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("/data/Test Convert DVD Subtitle (2019).mkv");
+
+        $this->assertEquals("subtitle", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
+        $this->assertEquals("subrip", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+        $this->assertEquals("eng", $probe["streams"][0]["tags"]["language"], "Stream 0 language");
+        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Convert DVD Subtitle", $probe["format"]["tags"]["title"], "Metadata title");
+    }
+
+    public function testBlacklist() {
+        $this->getFile("dvd");
+
+        $this->ripvideo(array("APPLY_POSTFIX"=>"false", "INPUT"=>"dvd.mkv", "TITLE"=>"Test Subtitle Files", "VIDEO_FORMAT"=>"copy", "AUDIO_TRACKS"=>-1, "SUBTITLE_FORMAT"=>"srt", "SUBTITLE_CONVERSION_OUTPUT"=>"FILE", "SUBTITLE_CONVERSION_BLACKLIST"=>"!\�~@~", "YEAR"=>2019), $output, $return);
+
+        $this->assertEquals(0, $return, "ripvideo exit code"); //test("ffmpeg code", 0, $return, $output);
+
+        $probe = $this->probe("Test Subtitle Files (2019).mkv");
+
+        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0");
+	$this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Subtitle Files", $probe["format"]["tags"]["title"], "Metadata Title");
+        $this->assertEquals("2019", $probe["format"]["tags"]["YEAR"], "Metadata Year");
+
+        $testfile = getEnv("DATA_DIR")."/Test Subtitle Files (2019).mkv.2-eng.srt";
+        $this->assertFileExists($testfile, "File missing");
+	$contents = file_get_contents($testfile);
+	$this->assertFalse(strpos($contents, "’"), "SRT contains '");
+	$this->assertFalse(strpos($contents, "!"), "SRT contains |");
+    }
+
+    public function testBluraySubtitles() {
+        $this->getFile("bluray.mkv");
+
+        $this->ripvideo(array("APPLY_POSTFIX"=>"false", "INPUT"=>"bluray.mkv", "TITLE"=>"Test Convert Bluray Subtitle", "VIDEO_TRACKS"=>-1, "AUDIO_TRACKS"=>-1, "YEAR"=>2019), $output, $return);
+
+        $this->assertEquals(0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("Test Convert Bluray Subtitle (2019).mkv");
+
+        $this->assertEquals("subtitle", $probe["streams"][0]["codec_type"], "Stream 0 code_type");
+        $this->assertEquals("ass", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+        $this->assertEquals("eng", $probe["streams"][0]["tags"]["language"], "Stream 0 language");
+        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
+        $this->assertEquals("Test Convert Bluray Subtitle", $probe["format"]["tags"]["title"], "Metadata title");
+    }
+}
+?>
