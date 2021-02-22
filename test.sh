c066a302376ff3b4fde6773921f0a20fa6a6db1e
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-22 09:16:20 -0500

    Use phpunit.xml for configs

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE} tests/
+
 TESTSUITES="basic,deinterlace,audio"
 if [[ "${BUILD_SUBTITLE_SUPPORT}" = "true" ]]; then
 	TESTSUITES="${TESTSUITES},subtitles"
 fi
 mkdir testResults
-set -ex
-#docker run --name test --user $(id -u):$(id -g) -v "$(pwd)/tests/:/tests/" -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
-docker run --rm -v "$(pwd)/testResults:/testResults" --user $(id -u):$(id -g) -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
+#docker run --rm -v "$(pwd)/testResults:/testResults" --user $(id -u):$(id -g) -e LOG_LEVEL=VERBOSE -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
+docker run --rm -v "$(pwd)/testResults:/testResults" --user $(id -u):$(id -g) -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
 cat testResults/testdox.txt
