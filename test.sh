5de0ab6bf38e72d8262551b824c325351665ac92
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-22 15:42:25 -0500

    Change the script to be a bit better

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Build beforehand
+# docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE} tests/
+
 set -e
-#docker build --tag test --build-arg BUILD_IMAGE=${THIS_FULL_IMAGE:?Missing THIS_FULL_IMAGE} tests/
 
 TESTSUITES="basic,deinterlace,audio"
 if [[ "${BUILD_SUBTITLE_SUPPORT}" = "true" ]]; then
@@ -9,13 +11,18 @@ if [[ "${BUILD_SUBTITLE_SUPPORT}" = "true" ]]; then
 fi
 mkdir testResults || true
 docker run --name test -d -v "$(pwd)/testResults:/testResults" --user $(id -u):$(id -g) -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN?Missing TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
-echo "Running tests..."
-sleep 10s
 PID=$(docker inspect test | grep "Pid\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
-tail --pid=${PID} -f testResults/testdox.txt
+while kill -0 ${PID} 2> /dev/null; do
+	sleep ${SLEEPTIME:-30s}
+	printf "%s Executing... " "$(date)"
+	tail -n1 testResults/testdox.txt
+done
 EXIT_CODE=$(docker inspect test | grep "ExitCode\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
 if [[ ${EXIT_CODE} -ne 0 ]]; then
 	docker logs test
 fi
 docker rm test
+printf "Test results\n"
+printf "============\n\n"
+cat testResults/testdox.txt
 exit ${EXIT_CODE}
