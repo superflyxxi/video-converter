f057066a9b64623640317f29021223bf219d7ce0
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-22 13:54:40 -0500

    Give it time to sleep

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ if [[ "${BUILD_SUBTITLE_SUPPORT}" = "true" ]]; then
 fi
 mkdir testResults || true
 docker run --name test -d -v "$(pwd)/testResults:/testResults" --user $(id -u):$(id -g) -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN?Missing TEST_SAMPLE_DOMAIN} test --testsuite ${TESTSUITES}
+echo "Running tests..."
+sleep 10s
 PID=$(docker inspect test | grep "Pid\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
 tail --pid=${PID} -f testResults/testdox.txt
 EXIT_CODE=$(docker inspect test | grep "ExitCode\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
