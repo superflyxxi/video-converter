c7d9d5abaafca38c36e94377592e061d1b53cddd
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-23 16:50:09 -0500

    Try not passing registry

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ if [[ ! -z "${CACHE_FROM_IMAGE}" ]]; then
   CACHE_FROM_ARGS="--cache-from ${CACHE_FROM_IMAGE}"
 fi
 
-docker build ${CACHE_FROM_ARGS} --build-arg BUILD_SUBTITLE_SUPPORT=${BUILD_SUBTITLE_SUPPORT:-true} --build-arg FROM_IMAGE=${FROM_IMAGE:?FROM_IMAGE missing} --tag ${THIS_FULL_IMAGE:?THIS_FULL_IMAGE missing} -f Dockerfile.${DOCKERFILE:?DOCKERFILE missing} .
+docker build ${CACHE_FROM_ARGS} --build-arg BUILD_SUBTITLE_SUPPORT=${BUILD_SUBTITLE_SUPPORT:-true} --build-arg FROM_IMAGE=${FROM_IMAGE:?FROM_IMAGE missing} --tag ${BUILD_IMAGE:?BUILD_IMAGE missing} -f Dockerfile.${DOCKERFILE:?DOCKERFILE missing} .
