19e3edad6b5780ad29f2ddd98c52d9d51005564c
SuperFlyXXI <superflyxxi@yahoo.com>
2021-02-25 09:19:03 -0500

    Moved mode testing to its own testsuite

--- tests/deinterlace/DeinterlaceTest.php
+++ tests/deinterlace/DeinterlaceTest.php
@@ -4,19 +4,15 @@ require_once "common.php";
 final class DeinterlacesTests extends Test
 {
 
-    public function testProbeAutoDeinterlaceAndMode00() {
+    public function testProbeAutoDeinterlace() {
         $this->getFile("dvd");
 
-        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Probe Auto Deinterlace", "YEAR"=>2019));
-
-// not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
-        $this->assertEquals(0, $return, "ripvideo exit code");
+        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Probe Auto Deinterlace", "YEAR"=>2019), "1m");
 
         $probe = $this->probe("Test Probe Auto Deinterlace (2019).dvd.mkv.mkv");
 
         $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
         $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-// doesn't exist... test("Stream 0 field_order", "progressive", $probe["streams"][0]["field_order"], $testOutput);
         $this->assertEquals("24000/1001", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
         $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
         $this->assertEquals("Test Probe Auto Deinterlace", $probe["format"]["tags"]["title"], "Metadata title");
@@ -26,12 +22,10 @@ final class DeinterlacesTests extends Test
         $this->assertFalse(array_key_exists("SUBTITLE", $probe["format"]["tags"]), "Metadata SUBTITLE");
     }
 
-    public function testIdetAutoDeinterlaceMode00() {
+    public function testIdetAutoDeinterlace() {
         $this->getFile("dvd");
 
-        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "DEINTERLACE_CHECK"=>"idet", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Idet Auto Deinterlace", "YEAR"=>2021));
-
-        $this->assertEquals(0, $return, "ripvideo exit code");
+        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE_MODE"=>"00", "DEINTERLACE_CHECK"=>"idet", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Idet Auto Deinterlace", "YEAR"=>2021), "1m");
 
         $probe = $this->probe("Test Idet Auto Deinterlace (2021).dvd.mkv.mkv");
 
@@ -40,41 +34,6 @@ final class DeinterlacesTests extends Test
         $this->assertEquals("24000/1001", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
         $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
         $this->assertEquals("Test Idet Auto Deinterlace", $probe["format"]["tags"]["title"], "Metadata title");
-        $this->assertEquals("2021", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
-    }
-
-    public function testDeinterlaceMode01() {
-        $this->getFile("dvd");
-
-        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE"=>"true", "DEINTERLACE_MODE"=>"01", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Deinterlace Mode 01", "YEAR"=>2021));
-
-// not validating return as it can be killed; test("ffmpeg code", 0, $return, $testOutput);
-        $this->assertEquals(0, $return, "ripvideo exit code");
-
-        $probe = $this->probe("Test Deinterlace Mode 01 (2021).dvd.mkv.mkv");
-
-        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
-        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-        $this->assertEquals("19001/317", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
-        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
-        $this->assertEquals("Test Deinterlace Mode 01", $probe["format"]["tags"]["title"], "Metadata title");
-        $this->assertEquals("2021", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
-    }
-
-    public function testDeinterlaceMode02() {
-        $this->getFile("dvd");
-
-        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "DEINTERLACE"=>"true", "DEINTERLACE_MODE"=>"02", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1, "TITLE"=>"Test Deinterlace Mode 02", "YEAR"=>2021));
-
-        $this->assertEquals(0, $return, "ripvideo exit code");
-
-        $probe = $this->probe("Test Deinterlace Mode 02 (2021).dvd.mkv.mkv");
-
-        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
-        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-        $this->assertEquals("30000/1001", $probe["streams"][0]["r_frame_rate"], "Stream 0 r_frame_rate");
-        $this->assertFalse(array_key_exists(1, $probe["streams"]), "Stream 1 exists");
-        $this->assertEquals("Test Deinterlace Mode 02", $probe["format"]["tags"]["title"], "Metadata title");
         $this->assertEquals("2021", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
     }
 }
