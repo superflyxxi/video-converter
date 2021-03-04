dea0523ed33e8f796b0c93e18d35af5d81e877f1
SuperFlyXXI <superflyxxi@yahoo.com>
2021-03-04 12:20:38 -0500

    Use it to find the country

--- php/CountryToLanguageMap.php
+++ php/CountryToLanguageMap.php
@@ -1,20 +1,20 @@
 <?php
 
-final class CountryToLanguageMap {
+final class CountryToLanguageMapping {
 
-  public static mapCountryLanguage = NULL;
+  public static mapLanguageCountry = NULL;
 
-  public static function getLanguage($country) {
-    if (array_key_exists($county, self::$mapCountryLanguage)) {
-      return self::$mapCountryLanguage[$country][0];
+  public static function getCountry($language) {
+    if (array_key_exists($language, self::$mapLanguageCountry)) {
+      return self::$mapLanguageCountry[$language][0];
     }
     return $country;
   }
 
   /** No need to execute this manually. Runs on load of class.**/
   public static function init() {
-    self::$mapCountryLanguage = array();
-    self::$mapCountryLanguage["fra"] = "fre";
+    self::$mapLanguageCountry = array();
+    self::$mapLanguageCountry["fre"] = "fra";
   }   
 }
 
