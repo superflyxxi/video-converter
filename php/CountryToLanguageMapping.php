2906e05acad2f9904d8d7e2e6a328118f135859c
SuperFlyXXI <superflyxxi@yahoo.com>
2021-03-06 15:22:04 -0500

    Include all languages

--- php/CountryToLanguageMapping.php
+++ php/CountryToLanguageMapping.php
@@ -14,7 +14,26 @@ final class CountryToLanguageMapping {
   /** No need to execute this manually. Runs on load of class.**/
   public static function init() {
     self::$mapLanguageCountry = array();
+    // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    self::$mapLanguageCountry["alb"] = "sqi";
+    self::$mapLanguageCountry["arm"] = "hye";
+    self::$mapLanguageCountry["baq"] = "eus";
+    self::$mapLanguageCountry["bur"] = "mya";
+    self::$mapLanguageCountry["chi"] = "zho";
+    self::$mapLanguageCountry["cze"] = "ces";
+    self::$mapLanguageCountry["dut"] = "nld";
     self::$mapLanguageCountry["fre"] = "fra";
+    self::$mapLanguageCountry["geo"] = "kat";
+    self::$mapLanguageCountry["ger"] = "deu";
+    self::$mapLanguageCountry["gre"] = "ell";
+    self::$mapLanguageCountry["ice"] = "isl";
+    self::$mapLanguageCountry["mac"] = "mkd";
+    self::$mapLanguageCountry["may"] = "msa";
+    self::$mapLanguageCountry["mao"] = "mri";
+    self::$mapLanguageCountry["per"] = "fas";
+    self::$mapLanguageCountry["slo"] = "slk";
+    self::$mapLanguageCountry["tib"] = "bod";
+    self::$mapLanguageCountry["wel"] = "cym";
   }   
 }
 
