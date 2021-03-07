b40a004db45d944b9e0126b5828e1d0c39102674
SuperFlyXXI <superflyxxi@yahoo.com>
2021-03-07 10:50:07 -0500

    Fixed stream number

--- tests/basic/BasicTest.php
+++ tests/basic/BasicTest.php
@@ -25,7 +25,7 @@ final class BasicTests extends Test
         $this->assertEquals("eng", $probe["streams"][2]["tags"]["language"], "Stream 2 language");
         $this->assertEquals("subtitle", $probe["streams"][3]["codec_type"], "Stream 3 codec_type");
         $this->assertEquals("dvd_subtitle", $probe["streams"][3]["codec_name"], "Stream 3 codec");
-        $this->assertEquals("fre", $probe["streams"][2]["tags"]["language"], "Stream 3 language");
+        $this->assertEquals("fre", $probe["streams"][3]["tags"]["language"], "Stream 3 language");
         $this->assertArrayNotHasKey(4, $probe["streams"], "Stream 4 exists");
         $this->assertEquals("Test No Input", $probe["format"]["tags"]["title"], "Metadata title");
         $this->assertEquals("2019", $probe["format"]["tags"]["YEAR"], "Metadata YEAR");
