6c0c7638c0d3893ac1df716998de4d76a91d287b
SuperFlyXXI <superflyxxi@yahoo.com>
2021-07-05 12:25:16 -0400

    LogWrapper construct

--- php/LogWrapper.php
+++ php/LogWrapper.php
@@ -6,7 +6,7 @@ use Monolog\Logger;
 
 class LogWrapper extends Logger
 {
-	__construct($name) {
+	public function __construct($name) {
 		parent::__construct($name);
 		$this->pushHandler(new StreamHandler('php://stdout', getEnvWithDefault("LOG_LEVEL", Logger::NOTICE)));
 	}
