8c5bf700602a2e6e7456af1d5dc0669ce5ff9ed8
SuperFlyXXI <superflyxxi@yahoo.com>
2021-07-05 10:18:34 -0400

    Use autoloader

--- php/rip-video.php
+++ php/rip-video.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
+require_once '/vendor/autoload.php';
 require_once "convert/ConvertFile.php";
 require_once "request/CSVRequest.php";
 
