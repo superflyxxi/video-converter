cd5d0db8e04ef44caa7ce10a2eaccd04703a48da
SuperFlyXXI <superflyxxi@yahoo.com>
2021-07-05 11:25:01 -0400

    Provide our own wrapper that initializes things

--- php/LogWrapper.php
+++ php/LogWrapper.php
@@ -4,36 +4,10 @@ require_once 'functions.php';
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
-$log = new Logger('video-converter');
-$log->pushHandler(new StreamHandler('php://stdout', getEnvWithDefault("LOG_LEVEL", Logger::NOTICE)));
-
-class Logger
+class LogWrapper extends Logger
 {
-
-    public static function info()
-    {
-        $log->info(func_get_args());
-    }
-
-    public static function warn()
-    {
-        $log->warn(func_get_args());
-    }
-
-    public static function error()
-    {
-        $log->error(func_get_args());
-    }
-
-    public static function verbose()
-    {
-        $log->debug(func_get_args());
-    }
-
-    public static function debug()
-    {
-        $log->debug(func_get_args());
-    }
+	__construct($name) {
+		parent::__construct($name);
+		$this->pushHandler(new StreamHandler('php://stdout', getEnvWithDefault("LOG_LEVEL", Logger::NOTICE)));
+	}
 }
-
-?>
