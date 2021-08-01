31d3582784377720c81d3b6f5337cd89274851ca
SuperFlyXXI <superflyxxi@yahoo.com>
2021-08-01 16:27:07 -0400

    Support using vaapi

--- test.sh
+++ test.sh
@@ -10,8 +10,17 @@ TESTSUITES=${TESTSUITES:-units,basic,deinterlace,audio,video}
 if [[ "${BUILD_SUBTITLE_SUPPORT}" = "true" ]]; then
 	TESTSUITES="${TESTSUITES},subtitles"
 fi
+if [[ "${USE_DEV_DRI:-false}" = "true" ]]; then
+	DEVICES="--device /dev/dri"
+fi
 mkdir testResults || true
-docker run --name test -d -v "$(pwd)/testResults:/testResults" --user $(id -u):$(id -g) -e LOG_LEVEL=100 -e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN?Missing TEST_SAMPLE_DOMAIN} ${TEST_IMAGE} --testsuite ${TESTSUITES}
+docker run --name test -d \
+	--user $(id -u):$(id -g) \
+	${DEVICES} \
+	-v "$(pwd)/testResults:/testResults" \
+	-e LOG_LEVEL=100 \
+	-e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN?Missing TEST_SAMPLE_DOMAIN} \
+	${TEST_IMAGE} --testsuite ${TESTSUITES}
 PID=$(docker inspect test | grep "Pid\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
 while kill -0 ${PID} 2> /dev/null; do
 	sleep ${SLEEPTIME:-30s}
