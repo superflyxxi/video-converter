88f77ee2c736e371ede2e0fe4a800f90265f789c
SuperFlyXXI <superflyxxi@yahoo.com>
2021-08-01 09:36:16 -0400

    Do 2.25 so that it's 1080

--- tests/video/UpscaleVaapiTest.php
+++ tests/video/UpscaleVaapiTest.php
@@ -7,17 +7,17 @@ final class UpscaleVaapiTests extends Test
     public function testUpscalingUsingVaapi() {
         $this->getFile("dvd");
 
-        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "TITLE"=>"Test 2x Vaapi Upscale", "VIDEO_UPSCALE"=>"2", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1));
+        $return = $this->ripvideo(array("INPUT"=>"dvd.mkv", "TITLE"=>"Test 2.25x Vaapi Upscale", "VIDEO_UPSCALE"=>"2.25", "AUDIO_TRACKS"=>-1, "SUBTITLE_TRACKS"=>-1));
         $this->assertEquals(0, $return, "ripvideo exit code");
 
-        $probe = $this->probe("Test 2x Vaapi Upscale.dvd.mkv.mkv");
+        $probe = $this->probe("Test 2.25x Vaapi Upscale.dvd.mkv.mkv");
 
         $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
         $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-        $this->assertEquals("1280", $probe["streams"][0]["width"], "Stream 0 width");
-        $this->assertEquals("720", $probe["streams"][0]["height"], "Stream 0 height");
+        $this->assertEquals("1440", $probe["streams"][0]["width"], "Stream 0 width");
+        $this->assertEquals("1080", $probe["streams"][0]["height"], "Stream 0 height");
         $this->assertArrayNotHasKey(1, $probe["streams"], "Stream 1 exists");
-        $this->assertEquals("Test 2x Vaapi Upscale", $probe["format"]["tags"]["title"], "Metadata title");
+        $this->assertEquals("Test 2.25x Vaapi Upscale", $probe["format"]["tags"]["title"], "Metadata title");
     }
 }
 ?>
