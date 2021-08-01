142784ba3d6e91e9e73217a15ed4299883ebaec3
SuperFlyXXI <superflyxxi@yahoo.com>
2021-08-01 09:52:35 -0400

    Initial test for inputfile

---
+++ tests/units/InputFileTest.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Tests when no input is given. It should product results for the file as expected.
+ */
+require_once "common.php";
+require_once "InputFile.php";
+
+final class InputFileTests extends Test
+{
+	public function testInputFileDVDmkv() {
+		$this->getFile("dvd");
+		$file = new InputFile($this->getDataDir() . DIRECTORY_SEPARATOR . "dvd.mkv");
+		$videoStreams = $file->getVideoStreams();
+		$this->assertEquals(1, count($videoStreams), "Video Streams");
+	}
+}
