6777cfb86cb9175964cee86d473e83ed0bdcfb57
SuperFlyXXI <superflyxxi@yahoo.com>
2021-08-01 15:35:41 -0400

    Fixed formatting

--- src/ffmpeg/generators/FFmpegVideoArgGenerator.php
+++ src/ffmpeg/generators/FFmpegVideoArgGenerator.php
@@ -7,68 +7,68 @@ require_once "Stream.php";
 class FFmpegVideoArgGenerator implements FFmpegArgGenerator
 {
 
-    public function getAdditionalArgs($outTrack, Request $request, $inputTrack, Stream $stream)
-    {
-        $args = " ";
-        if ("copy" == $request->videoFormat) {
-            $args .= " -c:v:" . $outTrack . " copy";
-        } else if ($request->isHDR()) {
-            $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10";
-        } else if ($request->isHwaccel()) {
-	    $filters = "";
-            if ($request->deinterlace) {
-                switch($request->deinterlaceMode) {
-                    default:
-                    case "00":
-                        $filters .= ",hwdownload,dejudder,fps=" . $stream->frame_rate .",fieldmatch,yadif=deint=interlaced,decimate,hwupload"; // https://ffmpeg.org/ffmpeg-filters.html#fieldmatch
-                        break;
-                    case "01":
-                        // each field is a frame (double framerate) https://www.mltframework.org/plugins/FilterAvfilter-deinterlace_vaapi/
-                        $filters .= ",deinterlace_vaapi=rate=field:auto=1"; 
-                        break;
-                    case "02":
-                        $filters .= ",deinterlace_vaapi";
-                        break;
-                }
-            }
-	    if ($request->videoUpscale != 1) {
-		$filtesr .= ",scale_vaapi=w=1280:h=720";
-	    }
-	    if (strlen($filters) > 0) {
-		$args .= " -vf '" . substr($filters, 1) . "'";
-	    }
-            $args .= " -c:v:" . $outTrack . " hevc_vaapi -qp 20 -level:v 4";
-        } else {
-	    $filters = "";
-            if ($request->deinterlace) {
-                switch($request->deinterlaceMode) {
-                    default:
-                    case "00":
-                        $filters .= ",dejudder,fps=" . $stream->frame_rate . ",fieldmatch,yadif=deint=interlaced,decimate"; // https://ffmpeg.org/ffmpeg-filters.html#fieldmatch
-                        break;
-                    case "01":
-                        $filters .= ",yadif=mode=1"; // each field is a frame (double framerate) https://ffmpeg.org/ffmpeg-filters.html#yadif-1
-                        break;
-                    case "02":
-                        $filters .= ",yadif"; // original
-                        break;
-                }
-            }
-	    if ($request->videoUpscale != 1) {
-		$filters .= ",scale=1280:720";
-	    }
-	    if (strlen($filters) > 0) {
-		$args .= " -vf '" . substr($filters, 1) . "'";
-	    }
-            $args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 4";
-        }
-        $args .= " -metadata:s:v:" . $outTrack . " language=" . $stream->language;
-        return $args;
-    }
+	public function getAdditionalArgs($outTrack, Request $request, $inputTrack, Stream $stream)
+	{
+		$args = " ";
+		if ("copy" == $request->videoFormat) {
+			$args .= " -c:v:" . $outTrack . " copy";
+		} else if ($request->isHDR()) {
+			$args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10";
+		} else if ($request->isHwaccel()) {
+			$filters = "";
+			if ($request->deinterlace) {
+				switch($request->deinterlaceMode) {
+					default:
+					case "00":
+						$filters .= ",hwdownload,dejudder,fps=" . $stream->frame_rate .",fieldmatch,yadif=deint=interlaced,decimate,hwupload"; // https://ffmpeg.org/ffmpeg-filters.html#fieldmatch
+						break;
+					case "01":
+						// each field is a frame (double framerate) https://www.mltframework.org/plugins/FilterAvfilter-deinterlace_vaapi/
+						$filters .= ",deinterlace_vaapi=rate=field:auto=1"; 
+						break;
+					case "02":
+						$filters .= ",deinterlace_vaapi";
+						break;
+				}
+			}
+			if ($request->videoUpscale != 1) {
+				$filters .= ",scale_vaapi=w=1280:h=720";
+			}
+			if (strlen($filters) > 0) {
+				$args .= " -vf '" . substr($filters, 1) . "'";
+			}
+			$args .= " -c:v:" . $outTrack . " hevc_vaapi -qp 20 -level:v 4";
+		} else {
+			$filters = "";
+			if ($request->deinterlace) {
+				switch($request->deinterlaceMode) {
+					default:
+					case "00":
+						$filters .= ",dejudder,fps=" . $stream->frame_rate . ",fieldmatch,yadif=deint=interlaced,decimate"; // https://ffmpeg.org/ffmpeg-filters.html#fieldmatch
+						break;
+					case "01":
+						$filters .= ",yadif=mode=1"; // each field is a frame (double framerate) https://ffmpeg.org/ffmpeg-filters.html#yadif-1
+						break;
+					case "02":
+						$filters .= ",yadif"; // original
+						break;
+				}
+			}
+			if ($request->videoUpscale != 1) {
+				$filters .= ",scale=1280:720";
+			}
+			if (strlen($filters) > 0) {
+				$args .= " -vf '" . substr($filters, 1) . "'";
+			}
+			$args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 4";
+		}
+		$args .= " -metadata:s:v:" . $outTrack . " language=" . $stream->language;
+		return $args;
+	}
 
-    public function getStreams(InputFile $inputFile)
-    {
-        return $inputFile->getVideoStreams();
-    }
+	public function getStreams(InputFile $inputFile)
+	{
+		return $inputFile->getVideoStreams();
+	}
 }
 ?>
