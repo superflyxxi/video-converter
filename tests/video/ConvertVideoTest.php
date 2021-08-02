7084139d15abf7622d90e2f1be1ccc5cd1322225
SuperFlyXXI <superflyxxi@yahoo.com>
2021-08-02 11:02:26 -0400

    Correct width

--- tests/video/ConvertVideoTest.php
+++ tests/video/ConvertVideoTest.php
@@ -14,7 +14,7 @@ final class ConvertVideoTests extends Test
 
 		$this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
 		$this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-		$this->assertEquals("420", $probe["streams"][0]["height"], "Stream 0 height");
+		$this->assertEquals("480", $probe["streams"][0]["height"], "Stream 0 height");
 		$this->assertEquals("720", $probe["streams"][0]["width"], "Stream 0 width");
 		$this->assertArrayNotHasKey(1, $probe["streams"], "Stream 1 exists");
 		$this->assertEquals("Test x265 Conversion", $probe["format"]["tags"]["title"], "Metadata title");
