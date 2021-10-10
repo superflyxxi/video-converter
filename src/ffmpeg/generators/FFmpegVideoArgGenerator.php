9032aad367239381b5d6786cda14a1d9dee5dc63
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-10 09:31:12 -0400

    Use constant

--- src/ffmpeg/generators/FFmpegVideoArgGenerator.php
+++ src/ffmpeg/generators/FFmpegVideoArgGenerator.php
@@ -6,14 +6,15 @@ require_once "Stream.php";
 
 class FFmpegVideoArgGenerator implements FFmpegArgGenerator
 {
+	private static $FFMPEG_CONVERT_ARG=" -c:v:";
 
 	public function getAdditionalArgs($outTrack, Request $request, $inputTrack, Stream $stream)
 	{
 		$args = " ";
 		if ("copy" == $request->videoFormat) {
-			$args .= " -c:v:" . $outTrack . " copy";
+			$args .= self::$FFMPEG_CONVERT_ARG . $outTrack . " copy";
 		} else if ($request->isHDR()) {
-			$args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10";
+			$args .= self::$FFMPEG_CONVERT_ARG . $outTrack . " libx265 -crf 20 -level:v 51 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 -profile:v main10";
 		} else if ($request->isHwaccel()) {
 			$filters = "";
 			if ($request->deinterlace) {
@@ -38,7 +39,7 @@ class FFmpegVideoArgGenerator implements FFmpegArgGenerator
 			if (strlen($filters) > 0) {
 				$args .= ' -vf "' . substr($filters, 1) . '"';
 			}
-			$args .= " -c:v:" . $outTrack . " hevc_vaapi -qp 20 -level:v 4";
+			$args .= self::$FFMPEG_CONVERT_ARG . $outTrack . " hevc_vaapi -qp 20 -level:v 4";
 		} else {
 			$filters = "";
 			if ($request->deinterlace) {
@@ -62,7 +63,7 @@ class FFmpegVideoArgGenerator implements FFmpegArgGenerator
 			if (strlen($filters) > 0) {
 				$args .= ' -vf "' . substr($filters, 1) . '"';
 			}
-			$args .= " -c:v:" . $outTrack . " libx265 -crf 20 -level:v 4";
+			$args .= self::$FFMPEG_CONVERT_ARG . $outTrack . " libx265 -crf 20 -level:v 4";
 		}
 		$args .= " -metadata:s:v:" . $outTrack . " language=" . $stream->language;
 		return $args;
