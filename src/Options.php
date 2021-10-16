9861750cac1f68e21994d492a07e7e3849e7ed9a
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-16 10:50:57 -0400

    Support env variables

--- src/Options.php
+++ src/Options.php
@@ -14,6 +14,10 @@ class Options
 
     public static function get($arg, $default)
     {
+	$env = getEnv(strtoupper(str_replace(array("-", "."), array("_", "__"), $arg)));
+	if ($env) {
+		return $env;
+	}
         if (array_key_exists($arg, self::$opts)) {
             return self::$opts[$arg];
         }
