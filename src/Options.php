4fe4389aa9058e7fadc28c7e3e85f9ecf27bdf54
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-16 11:05:50 -0400

    Fixed formatting

--- src/Options.php
+++ src/Options.php
@@ -6,17 +6,18 @@ class Options
     private static $log;
     private static $opts;
 
-    public static function init() {
+    public static function init()
+    {
         self::$log = new LogWrapper("Options");
         self::$opts = getopt("", ["log-level::"]);
     }
 
     public static function get($arg, $default)
     {
-	$env = getEnv(strtoupper(str_replace(array("-", "."), array("_", "__"), $arg)));
-	if ($env) {
-		return $env;
-	}
+        $env = getEnv(strtoupper(str_replace(["-", "."], ["_", "__"], $arg)));
+        if ($env) {
+            return $env;
+        }
         if (array_key_exists($arg, self::$opts)) {
             return self::$opts[$arg];
         }
