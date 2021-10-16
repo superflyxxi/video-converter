f0f34ab02cfa8fed14671ff66a643cfc5e4510f6
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-16 11:49:50 -0400

    Fix formatting

--- src/Options.php
+++ src/Options.php
@@ -1,26 +1,23 @@
 <?php
 require_once "LogWrapper.php";
 
-class Options
-{
-    private static $opts;
+class Options {
+	private static $opts;
 
-    public static function init()
-    {
-        self::$opts = getopt("", ["log-level::"]);
-    }
+	public static function init() {
+		self::$opts = getopt("", ["log-level::"]);
+	}
 
-    public static function get($arg, $default)
-    {
-        $env = getEnv(strtoupper(str_replace(["-", "."], ["_", "__"], $arg)));
-        if ($env) {
-            return $env;
-        }
-        if (array_key_exists($arg, self::$opts)) {
-            return self::$opts[$arg];
-        }
-        return $default;
-    }
+	public static function get($arg, $default) {
+		$env = getEnv(strtoupper(str_replace(["-", "."], ["_", "__"], $arg)));
+		if ($env) {
+			return $env;
+		}
+		if (array_key_exists($arg, self::$opts)) {
+			return self::$opts[$arg];
+		}
+		return $default;
+	}
 }
 Options::init();
 ?>
