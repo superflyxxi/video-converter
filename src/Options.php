5f470526c241666d2d75d9ee1c579328a8b352cb
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-16 10:25:25 -0400

    Basic options

---
+++ src/Options.php
@@ -0,0 +1,22 @@
+<?php
+require_once "LogWrapper.php";
+
+class Options {
+	private static $log;
+	private static $opts;
+
+	private static function init() {
+		self::$log = new LogWrapper("Options");
+		self::$opts = getopt("", array(""));
+	}
+
+	public static get($arg, $default) {
+		if (array_key_exists($arg, self::$opts)) {
+			return self::$opts[$arg];
+		}
+		return $default;
+	}
+}
+
+Options::init();
+?>
