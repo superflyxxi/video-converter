37f274d6f7ee5888fbbfbbfa69a02eb6d15b2ae6
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-17 10:19:07 -0400

    Fixed running tests

--- tests/common.php
+++ tests/common.php
@@ -76,7 +76,7 @@ abstract class Test extends TestCase {
 		foreach ($envVars as $key => $value) {
 			$command .= $key . '="' . $value . '" ';
 		}
-		$command .= "timeout -s15 " . $timeout . " /app/ripvideo/rip-video.php";
+		$command .= "timeout -s15 " . $timeout . " " . getEnv("SRC_DIR") . "/rip-video.php";
 		passthru($command, $return);
 		print "Return value " . $return;
 		return $return;
