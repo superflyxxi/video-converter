29eddca106739c890ce5abbf3b2fdc1704e86242
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-17 21:30:41 -0400

    Fixed formatting

--- src/Options.php
+++ src/Options.php
@@ -5,7 +5,34 @@ class Options {
 	private static $opts;
 
 	public static function init() {
-		self::$opts = getopt("", ["log-level::", "input::", "title:", "year::", "season::", "episode::", "show-title::", "video-tracks::", "video-format::", "video-upscale::", "deinterlace::", "deinterlace-check::", "audio-tracks::", "audio-format::", "audio-quality::", "audio-sample-rate::", "aduio-channel-layout::", "audio-channel-layout-tracks::", "normalize-audio-tracks::", "subtitle-tracks::", "subtitle-format::", "subtitle-conversion-output::", "subtitle-conversion-blacklist::", "hdr", "disable-postfix", "playlist::"]);
+		self::$opts = getopt("", [
+			"log-level::",
+			"input::",
+			"title:",
+			"year::",
+			"season::",
+			"episode::",
+			"show-title::",
+			"video-tracks::",
+			"video-format::",
+			"video-upscale::",
+			"deinterlace::",
+			"deinterlace-check::",
+			"audio-tracks::",
+			"audio-format::",
+			"audio-quality::",
+			"audio-sample-rate::",
+			"aduio-channel-layout::",
+			"audio-channel-layout-tracks::",
+			"normalize-audio-tracks::",
+			"subtitle-tracks::",
+			"subtitle-format::",
+			"subtitle-conversion-output::",
+			"subtitle-conversion-blacklist::",
+			"hdr",
+			"disable-postfix",
+			"playlist::",
+		]);
 	}
 
 	public static function get($arg, $default = null) {
