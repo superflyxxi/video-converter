7837d48a683db42ab7bc9c34d7c150890aea8570
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-18 18:35:03 -0400

    Run all tests

--- test.sh
+++ test.sh
@@ -11,14 +11,13 @@ if [[ "${USE_VAAPI:-false}" = "true" ]]; then
 	DEVICES="--device /dev/dri"
 fi
 mkdir testResults || true
-#	${TEST_IMAGE} --testsuite ${TESTSUITES} ${ADDITIONAL_PHPUNIT_ARGS}
 docker run --name test -d \
 	--user $(id -u):$(id -g) \
 	${DEVICES} \
 	-v "$(pwd)/testResults:/opt/video-converter/testResults" \
 	-e LOG_LEVEL=100 \
 	-e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN?Missing TEST_SAMPLE_DOMAIN} \
-	${TEST_IMAGE} ${ADDITIONAL_PHPUNIT_ARGS} tests/integration/SubtitleTest.php
+	${TEST_IMAGE} --testsuite ${TESTSUITES} ${ADDITIONAL_PHPUNIT_ARGS}
 PID=$(docker inspect test | grep "Pid\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
 while kill -0 ${PID} 2> /dev/null; do
 	sleep ${SLEEPTIME:-30s}
