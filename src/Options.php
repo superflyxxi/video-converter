8ac73e4ea51bec3cea1f8eed3ea0445f8a8bfcfa
SuperFlyXXI <superflyxxi@yahoo.com>
2021-10-19 15:06:17 -0400

    Fixed formatting

--- src/Options.php
+++ src/Options.php
@@ -6,33 +6,37 @@ class Options {
 
 	public static function init() {
 		$otherArgs = null;
-		self::$opts = getopt("", [
-			"log-level::",
-			"title:",
-			"year::",
-			"season::",
-			"episode::",
-			"show-title::",
-			"video-tracks::",
-			"video-format::",
-			"video-upscale::",
-			"deinterlace::",
-			"deinterlace-check::",
-			"audio-tracks::",
-			"audio-format::",
-			"audio-quality::",
-			"audio-sample-rate::",
-			"audio-channel-layout::",
-			"audio-channel-layout-tracks::",
-			"normalize-audio-tracks::",
-			"subtitle-tracks::",
-			"subtitle-format::",
-			"subtitle-conversion-output::",
-			"subtitle-conversion-blacklist::",
-			"hdr",
-			"disable-postfix",
-			"playlist::",
-		], $otherArgs);
+		self::$opts = getopt(
+			"",
+			[
+				"log-level::",
+				"title:",
+				"year::",
+				"season::",
+				"episode::",
+				"show-title::",
+				"video-tracks::",
+				"video-format::",
+				"video-upscale::",
+				"deinterlace::",
+				"deinterlace-check::",
+				"audio-tracks::",
+				"audio-format::",
+				"audio-quality::",
+				"audio-sample-rate::",
+				"audio-channel-layout::",
+				"audio-channel-layout-tracks::",
+				"normalize-audio-tracks::",
+				"subtitle-tracks::",
+				"subtitle-format::",
+				"subtitle-conversion-output::",
+				"subtitle-conversion-blacklist::",
+				"hdr",
+				"disable-postfix",
+				"playlist::",
+			],
+			$otherArgs
+		);
 		$otherArgs = array_slice($argv, $otherArgs);
 		self::$inputfile = empty($otherArgs) ? null : $otherArgs[0];
 	}
