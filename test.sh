ec870db52199498819674e12f3d7808e3e2431b3
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2021-10-20 21:17:00 -0400

    fix(ci): Run CricleCI tests in parallel (#130)
    * fix(ci): build images together and test running
    
    * Try parallelism
    
    * Show the by timings split
    
    * Split tests
    
    * Build after login to support caching
    
    * Extra debugging
    
    * Support tests files
    
    * Debugging
    
    * Fixed backwards logic
    
    * Works with tests
    
    * Support TESTS
    
    * Use TESTS
    
    * Split the tests
    
    * Replace \r
    
    * Fixed to do correct replacement
    
    * Separate job
    
    * Hard-code
    
    * Just run one job for now
    
    * Replace cr with space
    
    * Try different things
    
    * Help
    
    * Pull the image manually
    
    * Try something else
    
    * Do it ourselves
    
    * Only run my tests
    
    * Build array
    
    * Fixed some typos
    
    * Fixed i
    
    * try 3 parallel
    
    * Move to test.sh
    
    * Try running all of them
    
    * Extra info
    
    * Docker load if there
    
    * Only run on 0
    
    * Syntax
    
    * Verify odd counts
    
    * Add actual workflow
    
    * Use multiplier logic instead
    
    * Do 5 threads
    
    * Pre-download MKVs
    
    * Show ffprobe error
    
    * Move test-docker to where it was
    
    * Got back to basics
    
    * Tag the app image so test image can be built
    
    * Don't hard-code the primary tag
    
    * Silly mistake
    
    * Tune the docker image
    
    * Try without gzip
    
    * Removed commented out code

--- test.sh
+++ test.sh
@@ -5,19 +5,40 @@
 
 set -e
 
-TEST_IMAGE=${TEST_IMAGE:-test}
+TEST_IMAGE=${TEST_IMAGE:-video-converter-test}
 TESTSUITES=${TESTSUITES:-unit-tests,integration-tests}
+
+if [[ "" == "${CIRCLE_NODE_TOTAL}" ]]; then
+	TEST_ARG="--testsuite ${TESTSUITES}"
+else
+	printf "Using CircleCI nodes: %s out of %s\n\n" "${CIRCLE_NODE_INDEX}" "${CIRCLE_NODE_TOTAL}"
+	ALL_TESTS=$(docker run --rm -it ${TEST_IMAGE} --list-tests | grep "^\s*-" | sed 's/^\s*-\s*//g' | sed 's/\r\s*/\n/g')
+	ALL_TESTS=($ALL_TESTS)
+	multiplier=$(( ((10 * ${#ALL_TESTS[@]} / ${CIRCLE_NODE_TOTAL} ) + 5 ) / 10))
+	start=$((${CIRCLE_NODE_INDEX} * $multiplier))
+	end=$((( ${CIRCLE_NODE_INDEX} + 1 ) * $multiplier))
+	printf "Going %s (inclusive) to %s (exclusive) from %s total with multiplier=%s\n" "${start}" "${end}" "${len}" "$multiplier"
+	declare -a TESTS_TO_CONSIDER
+	for ((i=$start; i<$end; i++)); do
+		TESTS_TO_CONSIDER+=(${ALL_TESTS[$i]})
+	done
+	TESTS=${TESTS_TO_CONSIDER[@]}
+	TEST_ARG="--filter /${TESTS// /|}/"
+fi
+
 if [[ "${USE_VAAPI:-false}" = "true" ]]; then
 	DEVICES="--device /dev/dri"
 fi
+
 mkdir testResults || true
+
+set -x
 docker run --name test -d \
 	--user $(id -u):$(id -g) \
 	${DEVICES} \
 	-v "$(pwd)/testResults:/opt/video-converter/testResults" \
-	-e LOG_LEVEL=100 \
-	-e TEST_SAMPLE_DOMAIN=${TEST_SAMPLE_DOMAIN?Missing TEST_SAMPLE_DOMAIN} \
-	${TEST_IMAGE} --testsuite ${TESTSUITES} ${ADDITIONAL_PHPUNIT_ARGS}
+	${TEST_IMAGE} ${TEST_ARG} ${ADDITIONAL_PHPUNIT_ARGS}
+set +x
 PID=$(docker inspect test | grep "Pid\"" | sed 's/.*: \([0-9]\+\).*/\1/g')
 while kill -0 ${PID} 2> /dev/null; do
 	sleep ${SLEEPTIME:-30s}
