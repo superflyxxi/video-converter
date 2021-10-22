af9c9f63c439311198cedc44cb246594963c8884
superflyxxi <40075662+superflyxxi@users.noreply.github.com>
2021-10-22 09:51:16 -0400

    fix(ci): Use data providers for tests (#134)
    * Try data providers
    
    * Scaling data provider
    
    * Fixed syntax
    
    * Data provider for auto detection
    
    * Convert many things
    
    * Line progress for curl
    
    * Fixed formatting
    
    * TestCase can't override constructor
    
    * mkdir first
    
    * Fixed provider
    
    * Make provider a public function
    
    * Fixed expected
    
    * Give some labels
    
    * renamed test [ci skip]
    
    * Removed testsuites [ci skip]

--- tests/integration/SimpleScaleTest.php
+++ tests/integration/SimpleScaleTest.php
@@ -2,14 +2,22 @@
 require_once "common.php";
 
 final class SimpleScaleTest extends Test {
-	public function test_Simple_Upscaling() {
+	public function scaling(): array {
+		return ["Upscale" => ["1.5", "720", "1080"], "Downscale" => ["0.5", "240", "360"]];
+	}
+
+	/**
+	 * @test
+	 * @dataProvider scaling
+	 */
+	public function testScaling($factor, $height, $width) {
 		$this->getFile("dvd");
 
 		$return = $this->ripvideo(
 			"dvd.mkv",
 			[
-				"--title" => "Test 1.5x Simple Upscale",
-				"--video-upscale" => "1.5",
+				"--title" => "Test " . $factor . "x Scale",
+				"--video-upscale" => $factor,
 				"--audio-tracks" => -1,
 				"--subtitle-tracks" => -1,
 				"--deinterlace" => "off",
@@ -18,36 +26,14 @@ final class SimpleScaleTest extends Test {
 		);
 		$this->assertEquals(0, $return, "ripvideo exit code");
 
-		$probe = $this->probe("Test 1.5x Simple Upscale.dvd.mkv.mkv");
+		$probe = $this->probe("Test " . $factor . "x Scale.dvd.mkv.mkv");
 
 		$this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
 		$this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-		$this->assertEquals("720", $probe["streams"][0]["height"], "Stream 0 height");
-		$this->assertEquals("1080", $probe["streams"][0]["width"], "Stream 0 width");
+		$this->assertEquals($height, $probe["streams"][0]["height"], "Stream 0 height");
+		$this->assertEquals($width, $probe["streams"][0]["width"], "Stream 0 width");
 		$this->assertArrayNotHasKey(1, $probe["streams"], "Stream 1 exists");
-		$this->assertEquals("Test 1.5x Simple Upscale", $probe["format"]["tags"]["title"], "Metadata title");
-	}
-
-	public function test_Simple_Downscaling() {
-		$this->getFile("dvd");
-
-		$return = $this->ripvideo("dvd.mkv", [
-			"--title" => "Test 0.5x Downscale",
-			"--video-upscale" => "0.5",
-			"--audio-tracks" => -1,
-			"--subtitle-tracks" => -1,
-			"--deinterlace" => "off",
-		]);
-		$this->assertEquals(0, $return, "ripvideo exit code");
-
-		$probe = $this->probe("Test 0.5x Downscale.dvd.mkv.mkv");
-
-		$this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
-		$this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
-		$this->assertEquals("240", $probe["streams"][0]["height"], "Stream 0 height");
-		$this->assertEquals("360", $probe["streams"][0]["width"], "Stream 0 width");
-		$this->assertArrayNotHasKey(1, $probe["streams"], "Stream 1 exists");
-		$this->assertEquals("Test 0.5x Downscale", $probe["format"]["tags"]["title"], "Metadata title");
+		$this->assertEquals("Test " . $factor . "x Scale", $probe["format"]["tags"]["title"], "Metadata title");
 	}
 }
 ?>
