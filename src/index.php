2f52532600a201c4e76a58654479eac9a49886a3
SuperFlyXXI <superflyxxi@yahoo.com>
2021-12-02 11:44:23 -0500

    Moved some things to rip-video

--- src/index.php
+++ src/index.php
@@ -1,7 +1,4 @@
-#!/usr/bin/php
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
-require_once __DIR__ . "/../vendor/autoload.php";
 require_once "convert/ConvertFile.php";
 require_once "request/CSVRequest.php";
 require_once "LogWrapper.php";
--- src/rip-video.php
+++ src/rip-video.php
@@ -1,4 +1,7 @@
+#!/usr/bin/php
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
+require_once __DIR__ . "/../vendor/autoload.php";
 
 require_once "index.php";
 exit(rip());
