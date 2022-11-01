f98bafb3fbec6ff56ca6885afa14acd22c80f6c1
SuperFlyXXI <sueprflyxxi@yahoo.com>
2022-11-01 16:16:11 -0400

    Enable PSR2 to match SonarCloud

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,7 @@
 <?php
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-        '@PSR12' => true,
+        '@PSR2' => true,
         'braces' => ['position_after_functions_and_oop_constructs' => 'same'],
     ])->setIndent("\t")
 ;
