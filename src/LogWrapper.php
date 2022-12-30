baff524a720be6bc83e5835f3f6cbd92890de0f5
SuperFlyXXI <superflyxxi@yahoo.com>
2022-12-30 15:05:12 -0500

    fix: default logging to INFO level

--- src/LogWrapper.php
+++ src/LogWrapper.php
@@ -11,6 +11,6 @@ class LogWrapper extends Logger
     public function __construct($name)
     {
         parent::__construct($name);
-        $this->pushHandler(new StreamHandler("php://stdout", Options::get("log-level", Logger::NOTICE)));
+        $this->pushHandler(new StreamHandler("php://stdout", Options::get("log-level", Logger::INFO)));
     }
 }
