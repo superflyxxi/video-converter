7071efc7e690fbf53d48925b5c0321106422e738
SuperFlyXXI <superflyxxi@yahoo.com>
2023-07-02 14:43:01 -0400

    Only check number of channels, and not layout

--- tests/AudioTest.php
+++ tests/AudioTest.php
@@ -20,7 +20,7 @@ final class AudioTest extends Test
         $this->assertEquals(0, $return, "ripvideo exit code");
 
         $probe = $this->probe("Test Channel Mapping.dvd.mkv.mkv");
-        print_r($probe);
+
         $this->assertEquals("audio", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
         $this->assertEquals("aac", $probe["streams"][0]["codec_name"], "Steram 0 codec");
         $this->assertEquals("stereo", $probe["streams"][0]["channel_layout"], "Stream 0 channel_layout");
@@ -46,18 +46,15 @@ final class AudioTest extends Test
         $this->assertEquals(0, $return, "ripvideo exit code");
 
         $probe = $this->probe("Test Normalize Track 1 (2019).dvd.mkv.mkv");
-        print_r("Debugging probe");
-        print_r($probe);
+
         $this->assertEquals("audio", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
         $this->assertEquals("aac", $probe["streams"][0]["codec_name"], "Stream 0 codec");
         $this->assertEquals(6, $probe["streams"][0]["channels"], "Stream 0 channels");
-        $this->assertEquals("5.1", $probe["streams"][0]["channel_layout"], "Stream 0 channel_layout");
         $this->assertEquals("48000", $probe["streams"][0]["sample_rate"], "Stream 0 sample_rate");
         $this->assertEquals("Normalized Surround 5.1", $probe["streams"][1]["tags"]["title"], "Stream 1 title");
         $this->assertEquals("eng", $probe["streams"][1]["tags"]["language"], "Stream 1 language");
         $this->assertEquals("audio", $probe["streams"][1]["codec_type"], "Stream 1 codec_type");
         $this->assertEquals("aac", $probe["streams"][1]["codec_name"], "Stream 1 codec");
-        $this->assertEquals("5.1", $probe["streams"][1]["channel_layout"], "Stream 1 channel_layout");
         $this->assertEquals(6, $probe["streams"][1]["channels"], "Stream 1 channels");
         $this->assertEquals("48000", $probe["streams"][1]["sample_rate"], "Stream 1 sample_rate");
         $this->assertArrayNotHasKey(2, $probe["streams"], "Stream 2 exists");
