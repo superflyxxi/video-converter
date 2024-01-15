47baa428dcb583d60485bcdb529aa07490997956
SuperFlyXXI <superflyxxi@yahoo.com>
2024-01-15 11:28:16 -0500

    feat: dummy API

---
+++ js/services/task.js
@@ -0,0 +1,14 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import process from 'node:process';
+import NotFoundError from '../errors/not-found-error.js';
+//import validation from '../helpers/validation.js';
+
+const DATA_DIRECTORY = process.env.DATA_DIR ?? `${process.env.HOME}/data`;
+
+export async function getAllTasks() {
+	const result = [];
+	return result;
+}
+
+//export {getAllTasks};
