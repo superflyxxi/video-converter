b18d1f4336b01c366538e7a49e1c799496487748
SuperFlyXXI <superflyxxi@yahoo.com>
2024-01-22 09:12:27 -0500

    chore: interface

---
+++ src/SuperFlyXXI/VideoConverter/Normalizers/Normalizer.php
@@ -0,0 +1,9 @@
+<?php
+namespace SuperFlyXXI\VideoConverter\Normalizers;
+
+use SuperFlyXXI\VideoConverter\Requests\Request;
+
+interface Normalizer
+{
+    public function normalize(Request $oRequest, int $index): array;
+}
