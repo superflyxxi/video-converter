3b5bb70fb389b0c57b815b0c07b2f37950b9071f
SuperFlyXXI <superflyxxi@yahoo.com>
2024-01-28 15:59:03 -0500

    shutdown server on tests

--- unit-test/root.js
+++ unit-test/root.js
@@ -1,26 +1,23 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../src/js/index.js';
+import {app,server} from '../src/js/index.js';
 
 const {expect} = chai;
 
 chai.use(chaiHttp);
 
-describe('Root test', () => {
-	it('Should get 404', (done) => {
-		chai
-			.request(app)
-			.get('/')
-			.end((error, res) => {
-				expect(res).to.have.status(404);
-				expect(res.body).to.deep.include({
-					type: '/errors/NOT_FOUND',
-					title: 'Not Found',
-					status: res.status,
-					detail: 'GET / not a valid API.',
-				});
-				expect(res.body).to.have.property('instance');
-				done();
-			});
+describe('Root test', function() {
+	after(() => server.close());
+	
+	it('Should get 404', async function() {
+		const res = await chai.request(app).get('/').send();
+		expect(res).to.have.status(404);
+		expect(res.body).to.deep.include({
+			type: '/errors/NOT_FOUND',
+			title: 'Not Found',
+			status: res.status,
+			detail: 'GET / not a valid API.',
+		});
+		expect(res.body).to.have.property('instance');
 	});
 });
