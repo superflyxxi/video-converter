4c3cf1530f008efa6b3295136a9cd1af4cf74946
SuperFlyXXI <superflyxxi@yahoo.com>
2026-01-16 19:11:25 -0500

    test: quality variable bit rate

--- tests/ConvertVideoTest.php
+++ tests/ConvertVideoTest.php
@@ -27,4 +27,30 @@ final class ConvertVideoTest extends TestSetup
         $this->assertArrayNotHasKey(1, $probe["streams"], "Stream 1 exists");
         $this->assertEquals("Test x265 Conversion", $probe["format"]["tags"]["title"], "Metadata title");
     }
+
+    public function test_Quality_VBR()
+    {
+        $this->getFile("dvd");
+
+        $return = $this->ripvideo(
+            "dvd.mkv",
+            [
+                "--title" => "Test QVBR Conversion",
+                "--audio-tracks" => - 1,
+                "--subtitle-tracks" => - 1,
+        "--deinterlace" => "off",
+                "--video-rate-control-mode" => "qvbr"
+            ]
+        );
+        $this->assertEquals(0, $return, "ripvideo exit code");
+
+        $probe = $this->probe("Test QVBR Conversion.dvd.mkv.mkv");
+
+        $this->assertEquals("video", $probe["streams"][0]["codec_type"], "Stream 0 codec_type");
+        $this->assertEquals("hevc", $probe["streams"][0]["codec_name"], "Stream 0 codec");
+        $this->assertEquals("480", $probe["streams"][0]["height"], "Stream 0 height");
+        $this->assertEquals("720", $probe["streams"][0]["width"], "Stream 0 width");
+        $this->assertArrayNotHasKey(1, $probe["streams"], "Stream 1 exists");
+        $this->assertEquals("Test QVBR Conversion", $probe["format"]["tags"]["title"], "Metadata title");
+    }
 }
